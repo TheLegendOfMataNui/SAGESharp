41dcd488a5fb6edd890ed1179d49be62268ec899
Vraiment <jemc44@gmail.com>
2019-01-07 23:41:22 -0800

    Add skeleton for the UInt extensions and its tests

---
+++ SAGESharp/Extensions/UIntExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAGESharp.Extensions
+{
+    public static class UIntExtensions
+    {
+        /// <summary>
+        /// Gets an individual byte from the input value.
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// The starting position is zero.
+        /// </remarks>
+        /// 
+        /// <param name="value">The input value.</param>
+        /// <param name="bytePosition">The byte position to get.</param>
+        /// 
+        /// <returns>The byte in the given position of the input value.</returns>
+        /// 
+        /// <exception cref="ArgumentOutOfRangeException">If the byte position is out of range.</exception>
+        public static byte GetByte(this uint value, byte bytePosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sets an individual byte in a copy of the input value.
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// The starting position is zero.
+        /// </remarks>
+        /// 
+        /// <param name="value">The input value.</param>
+        /// <param name="bytePosition">The byte position to set.</param>
+        /// <param name="byteValue">The value that will be set.</param>
+        /// 
+        /// <returns>A copy of the input value with the byte in the given position updated.</returns>
+        /// 
+        /// <exception cref="ArgumentOutOfRangeException">If the byte position is out of range.</exception>
+        public static uint SetByte(this uint value, byte bytePosition, byte byteValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SAGESharpTests/Extensions/UIntExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using SAGESharp.Extensions;
+using System;
+
+namespace SAGESharpTests.Extensions
+{
+    [TestFixture]
+    public class UIntExtensionsTests
+    {
+        private const uint INPUT_NUMBER = 0xAABBCCDD;
+
+        [Test]
+        public void TestGetByteWithAValidBytePosition()
+        {
+            Assert.That(INPUT_NUMBER.GetByte(2), Is.EqualTo(0xBB));
+        }
+
+        [Test]
+        public void TestGetByteWithAnInvalidBytePosition()
+        {
+            Assert.That(() => INPUT_NUMBER.GetByte(4), Throws.InstanceOf(typeof(ArgumentOutOfRangeException)));
+        }
+
+        [Test]
+        public void TestSetByteWithAValidBytePosition()
+        {
+            Assert.That(INPUT_NUMBER.SetByte(2, 0xFF), Is.EqualTo(0xAAFFCCDD));
+        }
+
+        [Test]
+        public void TestSetByteWithAnInvalidBytePosition()
+        {
+            Assert.That(() => INPUT_NUMBER.SetByte(4, 0xFF), Throws.InstanceOf(typeof(ArgumentOutOfRangeException)));
+        }
+    }
+}
