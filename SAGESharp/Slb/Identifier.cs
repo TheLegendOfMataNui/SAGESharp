62109763a770628c2e1d1f328e8cb88f16177351
Vraiment <jemc44@gmail.com>
2019-01-07 21:59:17 -0800

    Add skeleton for the SLB Identifier struct and its tests

---
+++ SAGESharp/Slb/Identifier.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace SAGESharp.Slb
+{
+    /// <summary>
+    /// Class that represents an identifier within SLB files.
+    /// 
+    /// The identifier consist of 4 bytes/characters (a 32 bit integer).
+    /// </summary>
+    public struct Identifier
+    {
+        /// <summary>
+        /// Char that will be shown if any invalid byte is used in the identifier.
+        /// </summary>
+        public const char EMPY_CHAR = '?';
+
+        /// <summary>
+        /// Creates a new instance initializing it with the input value.
+        /// </summary>
+        /// <param name="value">The input value to initalize the instance.</param>
+        public Identifier(uint value)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The value of the identifier.
+        /// </summary>
+        public uint Value
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #region Byte level access
+        /// <summary>
+        /// Byte 0 of the id in character form.
+        /// </summary>
+        public char C0
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Byte 1 of the id in chracter form.
+        /// </summary>
+        public char C1
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Byte 2 of the id in chracter form.
+        /// </summary>
+        public char C2
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Byte 3 of the id in chracter form.
+        /// </summary>
+        public char C3
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Byte 0 of the id in numeric form.
+        /// </summary>
+        public byte B0
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Byte 1 of the id in numeric form.
+        /// </summary>
+        public byte B1
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Byte 2 of the id in numeric form.
+        /// </summary>
+        public byte B2
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Byte 3 of the id in numeric form.
+        /// </summary>
+        public byte B3
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Gets the identifier as a (4 character) string.
+        /// </summary>
+        /// 
+        /// <returns>The identifier as a (4 character) string.</returns>
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SAGESharpTests/Slb/IdentifierTests.cs
@@ -0,0 +1,116 @@
+﻿using NUnit.Framework;
+using SAGESharp.Slb;
+
+namespace SAGESharpTests.Slb
+{
+    [TestFixture]
+    public class IdentifierTests
+    {
+        [Test]
+        public void TestCreateEmptyIdentifier()
+        {
+            var identifier = new Identifier();
+            var expectedString = new string(Identifier.EMPY_CHAR, 4);
+
+            Assert.That(identifier.Value, Is.EqualTo(0));
+            Assert.That(identifier.ToString(), Is.EqualTo(expectedString));
+
+            Assert.That(identifier.C0, Is.EqualTo(Identifier.EMPY_CHAR));
+            Assert.That(identifier.C1, Is.EqualTo(Identifier.EMPY_CHAR));
+            Assert.That(identifier.C2, Is.EqualTo(Identifier.EMPY_CHAR));
+            Assert.That(identifier.C3, Is.EqualTo(Identifier.EMPY_CHAR));
+
+            Assert.That(identifier.B0, Is.EqualTo(0));
+            Assert.That(identifier.B1, Is.EqualTo(0));
+            Assert.That(identifier.B2, Is.EqualTo(0));
+            Assert.That(identifier.B3, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestCreateIdentifierFromInteger()
+        {
+            var identifier = new Identifier(0x44434241);
+
+            AssertIdentifierWithTestValue(identifier);
+        }
+
+        [Test]
+        public void TestSettingIdentifierValuesByValue()
+        {
+            var identifier = new Identifier
+            {
+                Value = 0x44434241
+            };
+
+            AssertIdentifierWithTestValue(identifier);
+        }
+
+        [Test]
+        public void TestSettingIdentifierValuesByChars()
+        {
+            var identifier = new Identifier
+            {
+                C0 = 'A',
+                C1 = 'B',
+                C2 = 'C',
+                C3 = 'D'
+            };
+
+            AssertIdentifierWithTestValue(identifier);
+        }
+
+        [Test]
+        public void TestSettingIdentifierValuesByBytes()
+        {
+            var identifier = new Identifier
+            {
+                B0 = 0x41,
+                B1 = 0x42,
+                B2 = 0x43,
+                B3 = 0x44
+            };
+
+            AssertIdentifierWithTestValue(identifier);
+        }
+
+        [Test]
+        public void TestSettingInvalidCharsShouldReturnEmptyChar()
+        {
+            var identifier = new Identifier();
+
+            identifier.B0 = 0x7B; // This is ASCII for '{'
+            Assert.That(identifier.C0, Is.EqualTo(Identifier.EMPY_CHAR));
+
+            identifier.B0 = 0x7C; // This is ASCII for '|'
+            Assert.That(identifier.C0, Is.EqualTo(Identifier.EMPY_CHAR));
+
+            identifier.B0 = 0x7D; // This is ASCII for '}'
+            Assert.That(identifier.C0, Is.EqualTo(Identifier.EMPY_CHAR));
+
+            identifier.B0 = 0x7E; // This is ASCII for '~'
+            Assert.That(identifier.C0, Is.EqualTo(Identifier.EMPY_CHAR));
+
+            var expectedString = new string(Identifier.EMPY_CHAR, 4);
+            Assert.That(identifier.ToString(), Is.EqualTo(expectedString));
+        }
+
+        private static void AssertIdentifierWithTestValue(Identifier identifier)
+        {
+            // Test value is the equivalent of:
+            // C0 = 'A', C1 = 'B', C2 = 'C', C3 = 'D'
+
+            Assert.That(identifier.Value, Is.EqualTo(0x44434241));
+            Assert.That(identifier.ToString(), Is.EqualTo("DCBA"));
+
+            Assert.That(identifier.C0, Is.EqualTo('A'));
+            Assert.That(identifier.C1, Is.EqualTo('B'));
+            Assert.That(identifier.C2, Is.EqualTo('C'));
+            Assert.That(identifier.C3, Is.EqualTo('D'));
+
+            Assert.That(identifier.B0, Is.EqualTo(0x41));
+            Assert.That(identifier.B1, Is.EqualTo(0x42));
+            Assert.That(identifier.B2, Is.EqualTo(0x43));
+            Assert.That(identifier.B3, Is.EqualTo(0x44));
+        }
+    }
+}
