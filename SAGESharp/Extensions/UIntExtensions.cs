cdbb7168a38dec57abcff8d91aa6c34f283df949
Vraiment <jemc44@gmail.com>
2019-01-09 20:32:54 -0800

    Make UInt extensions internal

--- SAGESharp/Extensions/UIntExtensions.cs
+++ SAGESharp/Extensions/UIntExtensions.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SAGESharp.Extensions
 {
-    public static class UIntExtensions
+    internal static class UIntExtensions
     {
         /// <summary>
         /// Gets an individual byte from the input value.
