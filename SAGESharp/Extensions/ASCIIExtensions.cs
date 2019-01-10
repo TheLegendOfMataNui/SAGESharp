9e4bccf0f3caca55f49566cc01eea2c4bac3b08e
Vraiment <jemc44@gmail.com>
2019-01-09 21:31:22 -0800

    Add extension methods for easy ASCII manipulation

---
+++ SAGESharp/Extensions/ASCIIExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace SAGESharp.Extensions
+{
+    internal static class ASCIIExtensions
+    {
+        /// <summary>
+        /// Converts the input byte to a char based on ASCII.
+        /// </summary>
+        /// 
+        /// <param name="b">The byte to convert.</param>
+        /// 
+        /// <returns>The byte in char form based on ASCII.</returns>
+        public static char ToASCIIChar(this byte b)
+        {
+            return Encoding.ASCII.GetChars(new[] { b })[0];
+        }
+
+        /// <summary>
+        /// Converts the input char to a byte based on ASCII.
+        /// </summary>
+        /// 
+        /// <param name="c">The char to convert.</param>
+        /// 
+        /// <returns>The char in byte form based on ASCII.</returns>
+        public static byte ToASCIIByte(this char c)
+        {
+            return Encoding.ASCII.GetBytes(new[] { c })[0];
+        }
+
+        /// <summary>
+        /// Returns true if the input byte is an ASCII value representing a digit.
+        /// </summary>
+        /// 
+        /// <param name="b">The input byte.</param>
+        /// 
+        /// <returns>True if the input byte is a value representing a digit, false otherwise.</returns>
+        public static bool IsASCIIDigit(this byte b)
+        {
+            return 0x30 <= b && b <= 0x39;
+        }
+
+        /// <summary>
+        /// Returns true if the input byte is an ASCII value representing an uppercase letter.
+        /// </summary>
+        /// 
+        /// <param name="b">The input byte.</param>
+        /// 
+        /// <returns>True if the input byte is a value representing an uppercase letter, false otherwise.</returns>
+        public static bool IsASCIIUppercaseLetter(this byte b)
+        {
+            return 0x41 <= b && b <= 0x5A;
+        }
+
+        /// <summary>
+        /// Returns true if the input byte is an ASCII value representing a lower case letter.
+        /// </summary>
+        /// 
+        /// <param name="b">The input byte.</param>
+        /// 
+        /// <returns>True if the input byte is a value representing a lower letter, false otherwise.</returns>
+        public static bool IsASCIILowercaseLetter(this byte b)
+        {
+            return 0x61 <= b && b <= 0x7A;
+        }
+    }
+}
---
+++ SAGESharpTests/Extensions/ASCIIExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using SAGESharp.Extensions;
+
+namespace SAGESharpTests.Extensions
+{
+    [TestFixture]
+    public class ASCIIExtensionsTests
+    {
+        private const byte RANDOM_BYTE = 0x2B;
+
+        private const char RANDOM_CHAR = '+';
+
+        [Test]
+        public void TestConvertingAByteToAnASCIIChar()
+        {
+            Assert.That(RANDOM_BYTE.ToASCIIChar(), Is.EqualTo(RANDOM_CHAR));
+        }
+
+        [Test]
+        public void TestConvertingACharToAnASCIIByte()
+        {
+            Assert.That(RANDOM_CHAR.ToASCIIByte(), Is.EqualTo(RANDOM_BYTE));
+        }
+
+        [Test]
+        public void TestIsAnASCIIDigitWithADigit()
+        {
+            byte value = 0x34;
+
+            Assert.That(value.IsASCIIDigit(), Is.True);
+        }
+
+        [Test]
+        public void TestIsAnASCIIDigitWithANonDigit()
+        {
+            Assert.That(RANDOM_BYTE.IsASCIIDigit(), Is.False);
+        }
+
+        [Test]
+        public void TestIsAnASCIIUppercaseLetterWithAnUppercaseLatter()
+        {
+            byte value = 0x48;
+
+            Assert.That(value.IsASCIIUppercaseLetter(), Is.True);
+        }
+
+        [Test]
+        public void TestIsAnASCIIUppercaseLetterWithANonUppercaseLatter()
+        {
+            Assert.That(RANDOM_BYTE.IsASCIIUppercaseLetter(), Is.False);
+        }
+
+        [Test]
+        public void TestIsAnASCIILowercaseLetterWithALowercaseLatter()
+        {
+            byte value = 0x70;
+
+            Assert.That(value.IsASCIILowercaseLetter(), Is.True);
+        }
+
+        [Test]
+        public void TestIsAnASCIILowerrcaseLetterWithANonUppercaseLatter()
+        {
+            Assert.That(RANDOM_BYTE.IsASCIILowercaseLetter(), Is.False);
+        }
+    }
+}
