7d4b4c4e445cf427df899bb66158fb87769ecbbc
Vraiment <jemc44@gmail.com>
2019-01-12 15:07:28 -0800

    Add ISlb interface for Slb IO related logic

---
+++ SAGESharp/Slb/ISlb.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace SAGESharp.Slb
+{
+    /// <summary>
+    /// Interface that represents an object in a SLB file.
+    /// </summary>
+    interface ISlb
+    {
+        /// <summary>
+        /// Reads the data from the given stream into the SLB object.
+        /// </summary>
+        /// 
+        /// <param name="stream">The stream from where the objects will be read.</param>
+        void ReadFrom(Stream stream);
+
+        /// <summary>
+        /// WRites the SLB object into the given stream.
+        /// </summary>
+        /// 
+        /// <param name="stream">The stream where the objects will be written.</param>
+        void WriteTo(Stream stream);
+    }
+}
