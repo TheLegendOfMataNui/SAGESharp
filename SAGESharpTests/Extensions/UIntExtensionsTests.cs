f2b2048090c073e10f982163f916f455a44bd2e3
Vraiment <jemc44@gmail.com>
2019-01-13 14:19:14 -0800

    Improve UInt extensions tests

--- SAGESharpTests/Extensions/UIntExtensionsTests.cs
+++ SAGESharpTests/Extensions/UIntExtensionsTests.cs
@@ -12,7 +12,10 @@ namespace SAGESharpTests.Extensions
         [Test]
         public void TestGetByteWithAValidBytePosition()
         {
+            Assert.That(INPUT_NUMBER.GetByte(0), Is.EqualTo(0xDD));
+            Assert.That(INPUT_NUMBER.GetByte(1), Is.EqualTo(0xCC));
             Assert.That(INPUT_NUMBER.GetByte(2), Is.EqualTo(0xBB));
+            Assert.That(INPUT_NUMBER.GetByte(3), Is.EqualTo(0xAA));
         }
 
         [Test]
@@ -24,7 +27,10 @@ namespace SAGESharpTests.Extensions
         [Test]
         public void TestSetByteWithAValidBytePosition()
         {
+            Assert.That(INPUT_NUMBER.SetByte(0, 0xFF), Is.EqualTo(0xAABBCCFF));
+            Assert.That(INPUT_NUMBER.SetByte(1, 0xFF), Is.EqualTo(0xAABBFFDD));
             Assert.That(INPUT_NUMBER.SetByte(2, 0xFF), Is.EqualTo(0xAAFFCCDD));
+            Assert.That(INPUT_NUMBER.SetByte(3, 0xFF), Is.EqualTo(0xFFBBCCDD));
         }
 
         [Test]
