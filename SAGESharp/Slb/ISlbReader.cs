6524530be3072bd239cfd9c7b6259d0cab474c99
Vraiment <jemc44@gmail.com>
2019-01-12 19:24:16 -0800

    Add ISlbReader and make ISLBObject public

---
+++ SAGESharp/Slb/ISlbReader.cs
@@ -0,0 +1,17 @@
+﻿namespace SAGESharp.Slb
+{
+    /// <summary>
+    /// Interface representing an object that reads SLB objects.
+    /// </summary>
+    /// 
+    /// <typeparam name="T">The type of SLB objects to read.</typeparam>
+    public interface ISlbReader<T> where T : ISlbObject
+    {
+        /// <summary>
+        /// Reads the SLB object.
+        /// </summary>
+        /// 
+        /// <returns>The SLB object that was read.</returns>
+        T ReadSlbObject();
+    }
+}
