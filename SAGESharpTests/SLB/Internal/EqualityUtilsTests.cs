c348f3563994f5da9f10937c045c42baccde1b1b
Vraiment <jemc44@gmail.com>
2019-01-26 01:05:28 -0800

    Add EqualityUtils extension methods
    These extension methods are needed in order to make easier to implement
    the `Equals()` and the `GetHashCode()` methods.
    
    This change also includes the `ParameterGroup` class which aims to make
    easier to implement unit tests using the `TestCaseSource` attribute.

---
+++ SAGESharpTests/SLB/Internal/EqualityUtilsTests.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using SAGESharp.SLB.Internal;
+using SAGESharpTests.Util;
+using System.Collections.Generic;
+
+namespace SAGESharpTests.SLB.Internal
+{
+    class EqualityUtilsTests
+    {
+        private const string STRING1 = "string1";
+        private const string STRING2 = "string2";
+
+        private static readonly int HASH_CODE1 = STRING1.GetHashCode();
+        private static readonly int HASH_CODE2 = STRING2.GetHashCode();
+
+        [TestCase(STRING1, STRING1, ExpectedResult = true)]
+        [TestCase(STRING1, STRING2, ExpectedResult = false)]
+        [TestCase(STRING2, STRING1, ExpectedResult = false)]
+        [TestCase(STRING2, STRING2, ExpectedResult = true)]
+        [TestCase(STRING1, STRING1, ExpectedResult = true)]
+        [TestCase(STRING1, null, ExpectedResult = false)]
+        [TestCase(null, STRING1, ExpectedResult = false)]
+        [TestCase(null, null, ExpectedResult = true)]
+        public bool Test_Is_Equals(string a, string b)
+        {
+            return a.SafeEquals(b);
+        }
+
+        [TestCaseSource(nameof(SingleReferences))]
+        public void Test_AddHashCodeByRef_For_A_Single_Reference(string value, int hash, int prime, int expectedHash)
+        {
+            value.AddHashCodeByRef(ref hash, prime);
+
+            Assert.That(hash, Is.EqualTo(expectedHash));
+        }
+
+        static object[] SingleReferences() => new ParameterGroup()
+            .Parameters(STRING1, 5, 7, 35 + STRING1.GetHashCode())
+            .Parameters(null, 5, 7, 35)
+            .Build();
+
+        [TestCaseSource(nameof(MultipleReferences))]
+        public void Test_AddHashCodesByRef_For_Multiple_References(IEnumerable<string> values, int hash, int prime, int emptyPrime, int expectedHash)
+        {
+            values.AddHashCodesByRef(ref hash, prime, emptyPrime);
+
+            Assert.That(hash, Is.EqualTo(expectedHash));
+        }
+
+        static object[] MultipleReferences() => new ParameterGroup()
+            // values, hash, prime, emptyPrime, expectedHash
+            .Parameters(null, 5, 7, 9, 5)
+            .Parameters(new List<string>(), 5, 7, 9, 45)
+            .Parameters(new List<string> { null }, 5, 7, 9, 35)
+            .Parameters(new List<string> { STRING1 }, 5, 7, 9, 35 + HASH_CODE1)
+            .Parameters(new List<string> { STRING1, null }, 5, 7, 9, (35 + HASH_CODE1) * 7)
+            .Parameters(new List<string> { STRING1, STRING2 }, 5, 7, 9, (35 + HASH_CODE1) * 7 + HASH_CODE2)
+            .Parameters(new List<string> { null, STRING1 }, 5, 7, 9, (35 * 7) + HASH_CODE1)
+            .Build();
+        
+        [TestCase(1, 5, 7, 36)]
+        public void Test_AddHashCodeByVal_For_A_Single_Value(int value, int hash, int prime, int expectedHash)
+        {
+            value.AddHashCodeByVal(ref hash, prime);
+
+            Assert.That(hash, Is.EqualTo(expectedHash));
+        }
+
+        [TestCaseSource(nameof(MultipleValues))]
+        public void Test_AddHashCodesByVal_For_Multiple_Values(IEnumerable<int> values, int hash, int prime, int emptyPrime, int expectedHash)
+        {
+            values.AddHashCodeByVal(ref hash, prime, emptyPrime);
+
+            Assert.That(hash, Is.EqualTo(expectedHash));
+        }
+
+        static object[] MultipleValues() => new ParameterGroup()
+            // values, hash, prime, emptyPrime, expectedHash
+            .Parameters(null, 5, 7, 9, 5)
+            .Parameters(new List<int>(), 5, 7, 9, 45)
+            .Parameters(new List<int> { 0 }, 5, 7, 9, 35)
+            .Parameters(new List<int> { 1 }, 5, 7, 9, 35 + 1)
+            .Parameters(new List<int> { 1, 0 }, 5, 7, 9, (35 + 1) * 7)
+            .Parameters(new List<int> { 1, 2 }, 5, 7, 9, (35 + 1) * 7 + 2)
+            .Parameters(new List<int> { 0, 1 }, 5, 7, 9, (35 * 7) + 1)
+            .Build();
+    }
+}
---
+++ SAGESharpTests/Util/ParameterGroup.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace SAGESharpTests.Util
+{
+    /// <summary>
+    /// Convenience class to use when creating arguments for a <see cref="TestCaseSourceAttribute"/>
+    /// </summary>
+    /// 
+    /// <example>
+    /// <code>
+    /// static object[] MyTestSource()
+    /// {
+    ///     return new ParameterGroup()
+    ///         .Parameters(1, "string1")
+    ///         .Parameters(4, null)
+    ///         .Parameters(35, "other string")
+    ///         .Build();
+    /// }
+    /// 
+    /// //...
+    /// 
+    /// [TestCase("MyTestSource")]
+    /// public void MyTestCase(int parameter1, string parameter2)
+    /// {
+    ///     // ...
+    /// }
+    /// </code>
+    /// </example>
+    internal class ParameterGroup
+    {
+        private readonly List<object> group = new List<object>();
+
+        /// <summary>
+        /// Add the list of parameters to the parameter group, least one is required.
+        /// </summary>
+        /// 
+        /// <param name="paramater">The first parameter to add.</param>
+        /// <param name="parameters">The list of parameters to add.</param>
+        /// 
+        /// <returns>The same parameter group to chain calls.</returns>
+        public ParameterGroup Parameters(object paramater, params object[] parameters)
+        {
+            // Merge the required parameter and the rest of the parameters.
+            object[] allParameters = new object[parameters.Length + 1];
+            allParameters[0] = paramater;
+            Array.Copy(parameters, 0, allParameters, 1, parameters.Length);
+
+            group.Add(allParameters);
+            return this;
+        }
+
+        /// <summary>
+        /// Build the object array for this parameter group.
+        /// </summary>
+        /// 
+        /// <returns>The object array for this parameter group.</returns>
+        public object[] Build()
+        {
+            return group.ToArray();
+        }
+    }
+}
