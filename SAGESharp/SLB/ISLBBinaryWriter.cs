0d448b3a7abdadd3c3f7da5c9b0dc0a04b3934f4
Vraiment <jemc44@gmail.com>
2019-02-09 19:06:39 -0800

    Add SLB.ISLBBinaryWriter interface

---
+++ SAGESharp/SLB/ISLBBinaryWriter.cs
@@ -0,0 +1,17 @@
+﻿namespace SAGESharp.SLB
+{
+    /// <summary>
+    /// Interface that represents an object to write SLB objects as binary data.
+    /// </summary>
+    /// 
+    /// <typeparam name="T">The type of SLB data that is going to be written.</typeparam>
+    internal interface ISLBBinaryWriter<T>
+    {
+        /// <summary>
+        /// Writes a SLB object into the underlying storage as binary data.
+        /// </summary>
+        /// 
+        /// <param name="slbObject">The SLB object that is going to be written as binary data.</param>
+        void WriteSLBObject(T slbObject);
+    }
+}
