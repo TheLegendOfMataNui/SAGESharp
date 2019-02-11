23a141cee38aa68ad2b6fffddeba3044f87a6fa1
Vraiment <jemc44@gmail.com>
2019-02-11 01:56:42 -0800

    Add SLBFooterWriter class and its tests

--- SAGESharp/SLB/Footer.cs
+++ SAGESharp/SLB/Footer.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using static System.BitConverter;
 
 namespace SAGESharp.SLB
 {
@@ -30,5 +34,75 @@ namespace SAGESharp.SLB
         /// <returns>The footer.</returns>
         /// <param name="slbObject">Slb object.</param>
         IDictionary<uint, uint> GenerateFooter(T slbObject);
+    }
+
+    /// <summary>
+    /// Implementation of the <see cref="ISLBFooterWriter{T}"/> interface.
+    /// </summary>
+    /// 
+    /// <typeparam name="T">The type of the SLB object.</typeparam>
+    internal class SLBFooterWriter<T> : ISLBFooterWriter<T>
+    {
+        private readonly Stream stream;
+
+        private readonly ISLBFooterGenerator<T> footerGenerator;
+
+        /// <summary>
+        /// Creates a new SLBFooterWriter.
+        /// </summary>
+        /// 
+        /// <param name="stream">The output stream.</param>
+        /// <param name="footerGenerator">The object that will generate the footer to write.</param>
+        /// 
+        /// <exception cref="ArgumentNullException">If any argument is null.</exception>
+        public SLBFooterWriter(Stream stream, ISLBFooterGenerator<T> footerGenerator)
+        {
+            this.stream = stream ?? throw new ArgumentNullException("The ouput stream cannot be null.");
+            this.footerGenerator = footerGenerator ?? throw new ArgumentNullException("The footer generator cannot be null");
+        }
+
+        /// <inheritdoc />
+        /// 
+        /// <exception cref="ArgumentNullException">If the input object is null.</exception>
+        public void WriteFooter(T slbObject)
+        {
+            if (slbObject == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var offsets = footerGenerator.GenerateFooter(slbObject);
+
+            foreach (var entry in offsets)
+            {
+                stream.Position = entry.Key;
+                stream.WriteUInt(entry.Value);
+            }
+
+            stream.Seek(0, SeekOrigin.End);
+
+            // Entries in the table (4 bytes/1 32 bit number per entry)
+            // + table size (4 bytes/1 32 bit number)
+            // + magic number (4 bytes/1 32 bit number)
+            var footerSize = (offsets.Count * 4) + 8;
+            var footer = new byte[footerSize];
+
+            var pos = 0;
+            foreach (var offset in offsets.Keys)
+            {
+                GetBytes(offset).CopyTo(footer, pos);
+                pos += 4;
+            }
+
+            GetBytes(offsets.Count).CopyTo(footer, pos);
+            pos += 4;
+
+            // Write magic numbers
+            footer[pos++] = 0xEE;
+            footer[pos++] = 0xFF;
+            footer[pos++] = 0xC0;
+
+            stream.Write(footer, 0, footerSize);
+        }
     }
 }
---
+++ SAGESharpTests/SLB/SLBWriterTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SAGESharp.SLB
+{
+    class SLBWriterTests
+    {
+        private readonly Stream stream;
+
+        private readonly ISLBFooterGenerator<object> footerGenerator;
+
+        private readonly ISLBFooterWriter<object> footerWriter;
+
+        public SLBWriterTests()
+        {
+            stream = Substitute.For<Stream>();
+            footerGenerator = Substitute.For<ISLBFooterGenerator<object>>();
+            footerWriter = new SLBFooterWriter<object>(stream, footerGenerator);
+        }
+
+        [Test]
+        public void Test_Creating_A_SLBFooterWriter_With_Null_Dependencies()
+        {
+            this.Invoking(_ => new SLBFooterWriter<object>(null, footerGenerator))
+                .Should()
+                .Throw<ArgumentNullException>();
+
+            this.Invoking(_ => new SLBFooterWriter<object>(stream, null))
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void Test_Writing_An_Aligned_Footer()
+        {
+            var dummy = new object();
+
+            footerGenerator.GenerateFooter(dummy).Returns(new Dictionary<uint, uint>()
+            {
+                { 0x01020304, 0x0A0B0C0D },
+                { 0x11121314, 0x1A1B1C1D },
+                { 0x21222324, 0x2A2B2C2D }
+            });
+
+            footerWriter.WriteFooter(dummy);
+
+            Received.InOrder(() =>
+            {
+                stream.Position = 0x01020304;
+                stream.Write(Matcher.ForEquivalentArray(new byte[] { 0x0D, 0x0C, 0x0B, 0x0A }), 0, 4);
+
+                stream.Position = 0x11121314;
+                stream.Write(Matcher.ForEquivalentArray(new byte[] { 0x1D, 0x1C, 0x1B, 0x1A }), 0, 4);
+
+                stream.Position = 0x21222324;
+                stream.Write(Matcher.ForEquivalentArray(new byte[] { 0x2D, 0x2C, 0x2B, 0x2A }), 0, 4);
+
+                stream.Seek(0, SeekOrigin.End);
+
+                var footer = new byte[]
+                {
+                    0x04, 0x03, 0x02, 0x01,
+                    0x14, 0x13, 0x12, 0x11,
+                    0x24, 0x23, 0x22, 0x21,
+                    0x03, 0x00, 0x00, 0x00,
+                    0xEE, 0xFF, 0xC0, 0x00
+                };
+
+                stream.Write(Matcher.ForEquivalentArray(footer), 0, footer.Length);
+            });
+        }
+    }
+}
