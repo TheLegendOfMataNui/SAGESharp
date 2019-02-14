05f8f8b89790d274dca2855090d42f6394855b04
Vraiment <jemc44@gmail.com>
2019-02-14 01:05:18 -0800

    Cleanup numeric extension tests

--- SAGESharpTests/SLB/NumericExtensionsTests.cs
+++ SAGESharpTests/SLB/NumericExtensionsTests.cs
@@ -1,42 +1,31 @@
-﻿using NUnit.Framework;
-using SAGESharp.SLB;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using System;
 
-namespace SAGESharpTests.SLB
+namespace SAGESharp.SLB
 {
-    [TestFixture]
-    public class NumericExtensionsTests
+    class NumericExtensionsTests
     {
-        private const uint INPUT_NUMBER = 0xAABBCCDD;
+        [TestCase(0xAABBCCDD, 0, ExpectedResult = 0xDD)]
+        [TestCase(0xAABBCCDD, 1, ExpectedResult = 0xCC)]
+        [TestCase(0xAABBCCDD, 2, ExpectedResult = 0xBB)]
+        [TestCase(0xAABBCCDD, 3, ExpectedResult = 0xAA)]
+        public byte Test_GetByte_With_A_Valid_Byte_Position(uint value, byte position)
+            => value.GetByte(position);
 
         [Test]
-        public void TestGetByteWithAValidBytePosition()
-        {
-            Assert.That(INPUT_NUMBER.GetByte(0), Is.EqualTo(0xDD));
-            Assert.That(INPUT_NUMBER.GetByte(1), Is.EqualTo(0xCC));
-            Assert.That(INPUT_NUMBER.GetByte(2), Is.EqualTo(0xBB));
-            Assert.That(INPUT_NUMBER.GetByte(3), Is.EqualTo(0xAA));
-        }
+        public void Test_GetByte_With_An_Invalid_Byte_Position()
+            => default(uint).Invoking(n => n.GetByte(5)).Should().Throw<ArgumentOutOfRangeException>();
+
+        [TestCase(0xAABBCCDD, 0, ExpectedResult = 0xAABBCCFF)]
+        [TestCase(0xAABBCCDD, 1, ExpectedResult = 0xAABBFFDD)]
+        [TestCase(0xAABBCCDD, 2, ExpectedResult = 0xAAFFCCDD)]
+        [TestCase(0xAABBCCDD, 3, ExpectedResult = 0xFFBBCCDD)]
+        public uint Test_SetByte_With_A_Valid_Byte_Position(uint value, byte position)
+            => value.SetByte(position, 0xFF);
 
         [Test]
-        public void TestGetByteWithAnInvalidBytePosition()
-        {
-            Assert.That(() => INPUT_NUMBER.GetByte(4), Throws.InstanceOf(typeof(ArgumentOutOfRangeException)));
-        }
-
-        [Test]
-        public void TestSetByteWithAValidBytePosition()
-        {
-            Assert.That(INPUT_NUMBER.SetByte(0, 0xFF), Is.EqualTo(0xAABBCCFF));
-            Assert.That(INPUT_NUMBER.SetByte(1, 0xFF), Is.EqualTo(0xAABBFFDD));
-            Assert.That(INPUT_NUMBER.SetByte(2, 0xFF), Is.EqualTo(0xAAFFCCDD));
-            Assert.That(INPUT_NUMBER.SetByte(3, 0xFF), Is.EqualTo(0xFFBBCCDD));
-        }
-
-        [Test]
-        public void TestSetByteWithAnInvalidBytePosition()
-        {
-            Assert.That(() => INPUT_NUMBER.SetByte(4, 0xFF), Throws.InstanceOf(typeof(ArgumentOutOfRangeException)));
-        }
+        public void Test_SetByte_With_An_Invalid_Byte_Position()
+            => default(uint).Invoking(n => n.SetByte(5, 0)).Should().Throw<ArgumentOutOfRangeException>();
     }
 }
