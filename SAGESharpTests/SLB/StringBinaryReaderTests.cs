e1ce00ba71ac8bfe2d405ce3bd66be7f51e1d996
Vraiment <jemc44@gmail.com>
2019-02-16 02:12:55 -0800

    Add SLB.StringBinaryReader class

---
+++ SAGESharpTests/SLB/StringBinaryReaderTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace SAGESharp.SLB
+{
+    public class StringBinaryReaderTests
+    {
+        private readonly Stream stream = Substitute.For<Stream>();
+
+        private readonly ISLBBinaryReader<string> reader;
+
+        public StringBinaryReaderTests()
+        {
+            reader = new StringBinaryReader(stream);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            stream.ClearReceivedCalls();
+        }
+
+        [Test]
+        public void Test_Creating_A_StringBinaryReader_With_Null_Dependencies()
+            => this
+                .Invoking(_ => new StringBinaryReader(null))
+                .Should()
+                .Throw<ArgumentNullException>();
+
+        [Test]
+        public void Test_Reading_A_String()
+        {
+            var expected = "A string"
+                .Select(c => c.ToASCIIByte())
+                .ToArray();
+            var length = expected.Length;
+
+            stream.ReadByte().Returns(length);
+            stream
+                .Read(Arg.Do<byte[]>(bytes => expected.CopyTo(bytes, 0)), 0, length)
+                .Returns(length);
+
+            reader.ReadSLBObject().Should().Be("A string");
+        }
+    }
+}
