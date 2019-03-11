6e803b1c380727b48d9f555a4d102e006eec051c
Vraiment <jemc44@gmail.com>
2019-03-10 18:43:07 -0700

    Add interfaces for generic binary reading

---
+++ SAGESharp/SLB/IO/BinarySerializerFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SAGESharp.SLB.IO
+{
+    /// <summary>
+    /// Interface to create instances of <see cref="IBinarySerializer"/>.
+    /// </summary>
+    public interface IBinarySerializerFactory
+    {
+        /// <summary>
+        /// Creates the corresponding <see cref="IBinarySerializer"/> instance for the given <paramref name="type"/>.
+        /// </summary>
+        /// 
+        /// <param name="type">The type to read.</param>
+        /// 
+        /// <returns>An instance of <see cref="IBinarySerializer" /> for <paramref name="type"/>.</returns>
+        IBinarySerializer GetSerializerForType(Type type);
+    }
+}
---
+++ SAGESharp/SLB/IO/BinarySerializers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SAGESharp.SLB.IO
+{
+    /// <summary>
+    /// Represents an object that can read other objects from a binary reader.
+    /// </summary>
+    public interface IBinarySerializer
+    {
+        /// <summary>
+        /// Reads an objects from the input <paramref name="binaryReader"/>.
+        /// </summary>
+        /// 
+        /// <param name="binaryReader">The binary reader used to read the object.</param>
+        /// 
+        /// <returns>The objec read from the <paramref name="binaryReader"/>.</returns>
+        object Read(IBinaryReader binaryReader);
+    }
+
+    /// <summary>
+    /// Specifies a property that should be serialized/deserialized for an SLB object.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public sealed class SLBElementAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new attribute to mark SLB elements.
+        /// </summary>
+        /// 
+        /// <param name="order">The order of the SLB property.</param>
+        public SLBElementAttribute(byte order) => Order = order;
+
+        /// <summary>
+        /// The order of the SLB property.
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// A single class/struct should not have two or more properties with the same value for Order.
+        /// </remarks>
+        public byte Order { get; private set; }
+    }
+}
