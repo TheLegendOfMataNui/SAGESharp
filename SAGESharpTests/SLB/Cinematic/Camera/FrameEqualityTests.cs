37229715586f4ec893d6d1382799bd776209e2c0
Vraiment <jemc44@gmail.com>
2019-03-24 23:35:52 -0700

    Add unit tests for new SLB classes

---
+++ SAGESharpTests/SLB/Cinematic/Camera/FrameEqualityTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using SAGESharp.Testing;
+using System;
+
+namespace SAGESharp.SLB.Cinematic.Camera
+{
+    class FrameEqualityTests : AbstractEqualityByRefTests<Frame>
+    {
+        protected override Frame GetDefault() => new Frame
+        {
+            Time = 1.5f,
+            Position = new Point3D { X = 1, Y = 2, Z = 3 },
+            Target = new Point3D { X = 4, Y = 5, Z = 6 },
+        };
+
+        protected override bool EqualsOperator(Frame left, Frame right)
+            => left == right;
+
+        protected override bool NotEqualsOperator(Frame left, Frame right)
+            => left != right;
+
+        [TestCaseSource(nameof(Modifiers))]
+        public void Test_Compare_Default_Object_With_Modified_Object(Action<Frame> modifier) =>
+            TestCompareDefaultObjectWithModifiedObject(modifier);
+
+        static object[] Modifiers() => new ParameterGroup<Action<Frame>>()
+            .Parameters(frame => frame.Time = 0)
+            .Parameters(frame => frame.Position = null)
+            .Parameters(frame => frame.Position.X = 0)
+            .Parameters(frame => frame.Target = null)
+            .Parameters(frame => frame.Target.X = 0)
+            .Build();
+    }
+}
