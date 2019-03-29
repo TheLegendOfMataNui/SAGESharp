cd55979cfdeec267690b0dcd674a9e364a66c604
Ben Garcia <pokeboygarcia@gmail.com>
2019-03-29 01:26:27 -0700

    Initial expression compilation!

--- SAGESharp/LSS/Compiler.cs
+++ SAGESharp/LSS/Compiler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SAGESharp.LSS.Expressions;
+using SAGESharp.LSS.Statements;
 using SAGESharp.OSI;
 
 namespace SAGESharp.LSS
@@ -22,55 +23,192 @@ namespace SAGESharp.LSS
             }
         }
 
-        public class SubroutineContext : Expressions.ExpressionVisitor<object, SubroutineContext>
+        private class SubroutineContext : ExpressionVisitor<object, object>, Statements.StatementVisitor<object>
         {
-            private List<Instruction> Instructions = new List<Instruction>();
+            public List<Instruction> Instructions { get; } = new List<Instruction>();
             private Dictionary<string, int> LocalLocations = new Dictionary<string, int>();
+            private OSIFile OSI { get; }
+            public bool IsFinalized { get; private set; }
 
-            public object VisitArrayAccessExpression(ArrayAccessExpression expr, SubroutineContext context)
+            public SubroutineContext(OSIFile osi, IEnumerable<string> parameterNames)
+            {
+                this.OSI = osi;
+                foreach (string param in parameterNames) {
+                    AddLocal(param);
+                }
+            }
+
+            public void FinalizeInstructions()
+            {
+                if (IsFinalized)
+                    throw new InvalidOperationException("Cannot modify a SubroutineContext after it has been finalized.");
+            }
+
+            private int AddLocal(string localName)
+            {
+                if (IsFinalized)
+                    throw new InvalidOperationException("Cannot modify a SubroutineContext after it has been finalized.");
+                int localIndex = LocalLocations.Count;
+                LocalLocations.Add(localName, localIndex);
+                return localIndex;
+            }
+
+            private ushort AddOrGetString(string value)
+            {
+                if (IsFinalized)
+                    throw new InvalidOperationException("Cannot modify a SubroutineContext after it has been finalized.");
+                int index = OSI.Strings.IndexOf(value);
+                if (index == -1)
+                {
+                    index = OSI.Strings.Count;
+                    OSI.Strings.Add(value);
+                }
+                return (ushort)index;
+            }
+
+            private string UnescapeString(string value)
+            {
+                return value.Replace("\\\"", "\"").Replace("\\n", "\n");
+            }
+
+            // TODO: Make a corresponding EscapeString and use it in OSIFile.ToString()
+
+            #region Expressions
+            public object VisitArrayAccessExpression(ArrayAccessExpression expr, object context)
             {
                 throw new NotImplementedException();
             }
 
-            public object VisitArrayExpression(ArrayExpression expr, SubroutineContext context)
+            public object VisitArrayExpression(ArrayExpression expr, object context)
             {
                 throw new NotImplementedException();
             }
 
-            public object VisitBinaryExpression(BinaryExpression expr, SubroutineContext context)
+            public object VisitBinaryExpression(BinaryExpression expr, object context)
             {
                 throw new NotImplementedException();
             }
 
-            public object VisitCallExpression(CallExpression expr, SubroutineContext context)
+            public object VisitCallExpression(CallExpression expr, object context)
             {
                 throw new NotImplementedException();
             }
 
-            public object VisitConstructorExpression(ConstructorExpression expr, SubroutineContext context)
+            public object VisitConstructorExpression(ConstructorExpression expr, object context)
             {
                 throw new NotImplementedException();
             }
 
-            public object VisitGroupingExpression(GroupingExpression expr, SubroutineContext context)
+            public object VisitGroupingExpression(GroupingExpression expr, object context)
+            {
+                return expr.Contents.AcceptVisitor(this, context);
+            }
+
+            public object VisitLiteralExpression(LiteralExpression expr, object context)
+            {
+                if (expr.Value.Type == TokenType.StringLiteral)
+                {
+                    ushort index = AddOrGetString(expr.Value.Content.Substring(1, expr.Value.Content.Length - 2));
+                    Instructions.Add(new BCLInstruction(BCLOpcode.PushConstantString, index));
+                }
+                else if (expr.Value.Type == TokenType.IntegerLiteral)
+                {
+                    int value = Int32.Parse(expr.Value.Content);
+                    if (value == 0)
+                    {
+                        Instructions.Add(new BCLInstruction(BCLOpcode.PushConstant0));
+                    }
+                    else if (value <= SByte.MaxValue && value >= SByte.MinValue)
+                    {
+                        Instructions.Add(new BCLInstruction(BCLOpcode.PushConstanti8, (sbyte)value));
+                    }
+                    else if (value <= Int16.MaxValue && value >= Int16.MinValue)
+                    {
+                        Instructions.Add(new BCLInstruction(BCLOpcode.PushConstanti16, (short)value));
+                    }
+                    else
+                    {
+                        Instructions.Add(new BCLInstruction(BCLOpcode.PushConstanti32, value));
+                    }
+                }
+                else if (expr.Value.Type == TokenType.FloatLiteral)
+                {
+                    Instructions.Add(new BCLInstruction(BCLOpcode.PushConstantf32, Single.Parse(expr.Value.Content)));
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid literal type: " + expr.Value.Type);
+                }
+                return null;
+            }
+
+            public object VisitUnaryExpression(UnaryExpression expr, object context)
             {
                 throw new NotImplementedException();
             }
 
-            public object VisitLiteralExpression(LiteralExpression expr, SubroutineContext context)
+            public object VisitVariableExpression(VariableExpression expr, object context)
+            {
+                throw new NotImplementedException();
+            }
+            #endregion
+
+            #region Statements
+            public object VisitBlockStatement(BlockStatement s)
             {
                 throw new NotImplementedException();
             }
 
-            public object VisitUnaryExpression(UnaryExpression expr, SubroutineContext context)
+            public object VisitClassStatement(ClassStatement s)
             {
                 throw new NotImplementedException();
             }
 
-            public object VisitVariableExpression(VariableExpression expr, SubroutineContext context)
+            public object VisitPropertyStatement(PropertyStatement s)
             {
                 throw new NotImplementedException();
             }
+
+            public object VisitSubroutineStatement(SubroutineStatement s)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object VisitGlobalStatement(GlobalStatement s)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object VisitExpressionStatement(ExpressionStatement s)
+            {
+                return s.Expression.AcceptVisitor(this, null);
+            }
+
+            public object VisitReturnStatement(ReturnStatement s)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object VisitIfStatement(IfStatement s)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object VisitWhileStatement(WhileStatement s)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object VisitAssignmentStatement(AssignmentStatement s)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object VisitVariableDeclarationStatement(VariableDeclarationStatement s)
+            {
+                throw new NotImplementedException();
+            }
+            #endregion
         }
 
         // Compiles a single source string into an OSI.
@@ -157,8 +295,10 @@ namespace SAGESharp.LSS
             Dictionary<string, ushort> symbols = new Dictionary<string, ushort>();
             Dictionary<string, ushort> globals = new Dictionary<string, ushort>();
 
-            List<string> functions = new List<string>();
-            List<string> classes = new List<string>();
+            //List<string> functions = new List<string>();
+            //List<string> classes = new List<string>();
+            Dictionary<string, OSIFile.FunctionInfo> functions = new Dictionary<string, OSIFile.FunctionInfo>();
+            Dictionary<string, OSIFile.ClassInfo> classes = new Dictionary<string, OSIFile.ClassInfo>();
 
             // Collect existing names
             for (int i = 0; i < result.OSI.Strings.Count; i++)
@@ -168,9 +308,9 @@ namespace SAGESharp.LSS
             for (int i = 0; i < result.OSI.Globals.Count; i++)
                 globals.Add(result.OSI.Globals[i], (ushort)i);
             for (int i = 0; i < result.OSI.Functions.Count; i++)
-                functions.Add(result.OSI.Functions[i].Name);
+                functions.Add(result.OSI.Functions[i].Name, result.OSI.Functions[i]);
             for (int i = 0; i < result.OSI.Classes.Count; i++)
-                classes.Add(result.OSI.Classes[i].Name);
+                classes.Add(result.OSI.Classes[i].Name, result.OSI.Classes[i]);
 
             // Add new symbols, globals, class info, and method info
             foreach (Parser.Result parseResult in parseResults)
@@ -179,7 +319,7 @@ namespace SAGESharp.LSS
                 {
                     List<ushort> propertySymbols = new List<ushort>();
                     List<OSIFile.MethodInfo> methods = new List<OSIFile.MethodInfo>();
-                    if (classes.Contains(cls.Name.Content))
+                    if (classes.ContainsKey(cls.Name.Content))
                     {
                         result.Errors.Add(new SyntaxError("Class already exists with same name.", cls.Name.SourceLocation.Offset, cls.Name.SourceLength, 0));
                     }
@@ -219,13 +359,15 @@ namespace SAGESharp.LSS
 
                 foreach (Statements.SubroutineStatement func in parseResult.Functions)
                 {
-                    if (functions.Contains(func.Name.Content))
+                    if (functions.ContainsKey(func.Name.Content))
                     {
                         result.Errors.Add(new SyntaxError("Function already exists with same name.", func.Name.SourceLocation.Offset, func.Name.SourceLength, 0));
                     }
                     else
                     {
-                        result.OSI.Functions.Add(new OSIFile.FunctionInfo(func.Name.Content, (ushort)func.Parameters.Count));
+                        OSIFile.FunctionInfo newFunc = new OSIFile.FunctionInfo(func.Name.Content, (ushort)func.Parameters.Count);
+                        functions.Add(func.Name.Content, newFunc);
+                        result.OSI.Functions.Add(newFunc);
                     }
                 }
 
@@ -245,7 +387,21 @@ namespace SAGESharp.LSS
                     }
                 }
 
-                // TODO: Compile the subroutines
+                // Compile the functions
+                foreach (SubroutineStatement function in parseResult.Functions)
+                {
+                    SubroutineContext context = new SubroutineContext(result.OSI, function.Parameters.Select(token => token.Content));
+                    foreach (InstructionStatement stmt in function.Body.Instructions)
+                    {
+                        stmt.AcceptVisitor(context);
+                    }
+                    context.FinalizeInstructions();
+                    OSIFile.FunctionInfo destination = functions[function.Name.Content];
+                    destination.Instructions.Clear();
+                    destination.Instructions.AddRange(context.Instructions);
+                }
+
+                // Compile the methods
             }
         }
 
