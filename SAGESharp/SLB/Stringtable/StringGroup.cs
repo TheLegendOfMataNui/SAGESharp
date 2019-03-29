6f420c7333bbc801686c8ce782a93df5b756f332
OndrikB <38321117+OndrikB@users.noreply.github.com>
2019-03-29 23:01:49 +0100

    Added stringtable class

---
+++ SAGESharp/SLB/Stringtable/StringGroup.cs
@@ -0,0 +1,62 @@
+using Konvenience;
+using SAGESharp.SLB.IO;
+using System;
+using System.Collections.Generic;
+
+namespace SAGESharp.SLB.Stringtable
+{
+    public sealed class StringGroup : IEquatable<StringGroup>
+    {
+        [SLBElement(1)]
+        public StringTableID Id { get; set; }
+
+        [SLBElement(2)]
+        public IList<Pstringn> Strings { get; set; }
+
+
+        public bool Equals(StringGroup other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Id == other.Id &&
+                StrOffset == other.StrOffset;
+        }
+
+        public override string ToString() => $"Id={Id}," +
+            $"Strings={Strings?.Let(Strings => "[(" + string.Join("), (", Strings) + ")]") ?? "null"}";
+
+        public override bool Equals(object other)
+            => Equals(other as StringGroup);
+
+        public override int GetHashCode()
+        {
+            int hash = 9857;
+            Id.AddHashCodeByVal(ref hash, 4523);
+            Strings.AddHashCodesByRef(ref hash, 9623, 2347);
+
+            return hash;
+        }
+
+        public static bool operator ==(StringGroup left, StringGroup right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+            else if (left is null)
+            {
+                return right.Equals(left);
+            }
+            else
+            {
+                return left.Equals(right);
+            }
+        }
+
+        public static bool operator !=(StringGroup left, StringGroup right)
+            => !(left == right);
+    }
+}
---
+++ SAGESharp/SLB/Stringtable/StringTable.cs
@@ -0,0 +1,55 @@
+using Konvenience;
+using SAGESharp.SLB.IO;
+using System;
+using System.Collections.Generic;
+
+namespace SAGESharp.SLB.Stringtable
+{
+    public sealed class StringTable : IEquatable<StringTable>
+    {
+        [SLBElement(1)]
+        public IList<StringGroup> StringGroups { get; set; }
+
+
+        public bool Equals(StringTable other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return StringGroups.SafeSequenceEquals(other.StringGroups);
+        }
+
+        public override string ToString() => $"StringGroups={StringGroups?.Let(StringGroups => "[(" + string.Join("), (", StringGroups) + ")]") ?? "null"}";
+
+        public override bool Equals(object other)
+            => Equals(other as StringGroup);
+
+        public override int GetHashCode()
+        {
+            int hash = 7723;
+            StringGroups.AddHashCodesByRef(ref hash, 9623, 2347);
+            return hash;
+        }
+
+        public static bool operator ==(StringTable left, StringTable right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+            else if (left is null)
+            {
+                return right.Equals(left);
+            }
+            else
+            {
+                return left.Equals(right);
+            }
+        }
+
+        public static bool operator !=(StringTable left, StringTable right)
+            => !(left == right);
+    }
+}
