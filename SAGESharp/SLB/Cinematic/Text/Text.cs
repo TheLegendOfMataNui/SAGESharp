2c86b41f77e012407e5f9a17cf6550772c8cfe97
OndrikB <38321117+OndrikB@users.noreply.github.com>
2019-03-29 22:45:17 +0100

    cin_text added

---
+++ SAGESharp/SLB/Cinematic/Text/Text.cs
@@ -0,0 +1,68 @@
+using Konvenience;
+using SAGESharp.SLB.IO;
+using System;
+using System.Collections.Generic;
+
+namespace SAGESharp.SLB.Cinematic.Text
+{
+    public sealed class Text : IEquatable<Text>
+    {
+        [SLBElement(1)]
+        public Identifier StrLabel { get; set; }
+
+        [SLBElement(2)]
+        public long StrIdx { get; set; }
+
+        [SLBElement(3)]
+        public IList<Data> Entries { get; set; }
+
+
+        public bool Equals(Text other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Instance == other.Instance &&
+                StrIdx == other.StrIdx &&
+                Entries.SafeSequenceEquals(other.Entries);
+        }
+
+        public override string ToString() => $"StrLabel={StrLabel}," +
+            $"StrIdx={StrIdx}," +
+            $"Locations={Entries?.Let(Entries => "[(" + string.Join("), (", Entries) + ")]") ?? "null"}";
+
+        public override bool Equals(object other)
+            => Equals(other as Text);
+
+        public override int GetHashCode()
+        {
+            int hash = 3299;
+            Instance.AddHashCodeByVal(ref hash, 2957);
+            StrIdx.AddHashCodeByVal(ref hash, 2957);
+            Entries.AddHashCodesByRef(ref hash, 2957, 4093);
+
+            return hash;
+        }
+
+        public static bool operator ==(Text left, Text right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+            else if (left is null)
+            {
+                return right.Equals(left);
+            }
+            else
+            {
+                return left.Equals(right);
+            }
+        }
+
+        public static bool operator !=(Text left, Text right)
+            => !(left == right);
+    }
+}
