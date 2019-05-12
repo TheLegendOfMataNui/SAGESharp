6960876db7610244f7ffd441ace9d9cd572e56e1
Vraiment <jemc44@gmail.com>
2019-05-12 13:53:42 -0700

    Add code to facilitate validations/throwing exceptions

---
+++ SAGESharp/Validations.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using System;
+
+namespace SAGESharp
+{
+    internal static class Validate
+    {
+        /// <summary>
+        /// Validates that <paramref name="argument"/> is not null,
+        /// or else throws an <see cref="ArgumentNullException"/> with the given <paramref name="argumentName"/>.
+        /// </summary>
+        /// 
+        /// <typeparam name="T">The type of the argument to check.</typeparam>
+        /// 
+        /// <param name="argumentName">The name of the argument to check.</param>
+        /// <param name="argument">The argument to check.</param>
+        public static void ArgumentNotNull<T>(string argumentName, T argument) where T : class
+        {
+            if (argument is null)
+            {
+                throw new ArgumentNullException($"\"{argumentName}\" cannot be null.");
+            }
+        }
+    }
+}
---
+++ SAGESharpTests/ValidationsTests.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace SAGESharp
+{
+    class ValidationsTests
+    {
+        [Test]
+        public void Test_Validate_ArgumentNotNull_With_A_Not_Null_Argument()
+            => new object().Invoking(o => Validate.ArgumentNotNull("arg", o))
+                .Should()
+                .NotThrow<ArgumentNullException>();
+
+        [Test]
+        public void Test_Validate_ArgumentNotNull_With_A_Null_Argument()
+        {
+            string argumentName = "arg";
+            Action action = () => Validate.ArgumentNotNull<object>(argumentName, null);
+
+            action
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithMessage($"*{argumentName}*");
+        }
+    }
+}
