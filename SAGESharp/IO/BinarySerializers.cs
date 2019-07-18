d1ae690baca9fb3f5ce2f9c25d8b1743956e9389
Vraiment <jemc44@gmail.com>
2019-07-18 10:48:06 -0700

    Move SerializablePropertyAttribute to an "Attributes" file

--- SAGESharp/IO/BinarySerializers.cs
+++ SAGESharp/IO/BinarySerializers.cs
@@ -143,29 +143,6 @@ namespace SAGESharp.IO
             => new BadTypeException(typeof(T), message, innerException);
     }
 
-    /// <summary>
-    /// Specifies a property that should be serialized/deserialized.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
-    public sealed class SerializablePropertyAttribute : Attribute
-    {
-        /// <summary>
-        /// Initializes a new attribute to mark a serializable property.
-        /// </summary>
-        /// 
-        /// <param name="binaryOrder">The binary order of property (see <see cref="BinaryOrder"/>).</param>
-        public SerializablePropertyAttribute(byte binaryOrder) => BinaryOrder = binaryOrder;
-
-        /// <summary>
-        /// The order to serialize/deserialize the property as binary data.
-        /// </summary>
-        /// 
-        /// <remarks>
-        /// A single class/struct should not have duplicated values for <see cref="BinaryOrder"/>.
-        /// </remarks>
-        public byte BinaryOrder { get; private set; }
-    }
-
     internal sealed class PrimitiveBinarySerializer<T> : IBinarySerializer<T>
     {
         public T Read(IBinaryReader binaryReader)
