90a8212ae989b577d9de1ec8f7fd9852bdf7aca6
Vraiment <jemc44@gmail.com>
2019-07-24 10:32:21 -0700

    Add definitions for template SLB_targets.bt

---
+++ SAGESharp/SLB/Targets.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using SAGESharp.IO;
+using System.Collections.Generic;
+
+namespace SAGESharp.SLB
+{
+    public sealed class Targets
+    {
+        [SerializableProperty(1)]
+        [DuplicateEntryCount]
+        public IList<Target> Entries { get; set; }
+    }
+
+    public sealed class Target
+    {
+        [SerializableProperty(1)]
+        public Identifier Id { get; set; }
+
+        [SerializableProperty(2)]
+        public int Index { get; set; }
+    }
+}
