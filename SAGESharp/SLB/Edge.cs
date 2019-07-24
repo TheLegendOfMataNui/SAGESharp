435beb66a3f192c7a8e039252472e198506e9bf1
Vraiment <jemc44@gmail.com>
2019-07-24 00:12:41 -0700

    Add definitions for template SLB_edge.bt

---
+++ SAGESharp/SLB/Edge.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using SAGESharp.IO;
+using System.Collections.Generic;
+
+namespace SAGESharp.SLB
+{
+    public sealed class EdgeTable
+    {
+        [SerializableProperty(1)]
+        public IList<Edge> Entries { get; set; }
+    }
+
+    public sealed class Edge
+    {
+        [SerializableProperty(1)]
+        public Identifier Id { get; set; }
+
+        [SerializableProperty(2)]
+        public IList<EdgeData> EdgeData { get; set; }
+    }
+
+    public sealed class EdgeData
+    {
+        [SerializableProperty(1)]
+        public float A { get; set; }
+
+        [SerializableProperty(2)]
+        public float B { get; set; }
+    }
+}
