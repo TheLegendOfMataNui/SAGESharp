1f47830f202f501f0aad28cc19a262e5ae876dd9
Vraiment <jemc44@gmail.com>
2019-07-24 00:22:42 -0700

    Add definitions for template SLB_level_cam.bt and SLB_level_spline.bt

---
+++ SAGESharp/SLB/Level/Camera.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using SAGESharp.IO;
+using System.Collections.Generic;
+
+namespace SAGESharp.SLB.Level
+{
+    public sealed class SPLinePathsTable
+    {
+        [SerializableProperty(1)]
+        public IList<SPLinePath> Entries { get; set; }
+    }
+}
