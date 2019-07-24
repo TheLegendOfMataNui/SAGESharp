23ac1f051f45de3b95ee30930b6ef65b5e7ffd68
Vraiment <jemc44@gmail.com>
2019-07-24 00:45:25 -0700

    Add definitions for template SLB_material.bt

---
+++ SAGESharp/SLB/Material.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using SAGESharp.IO;
+using System.Collections.Generic;
+
+namespace SAGESharp.SLB
+{
+    public sealed class MaterialTable
+    {
+        [SerializableProperty(1)]
+        public IList<Material> Entries { get; set; }
+    }
+
+    public sealed class Material
+    {
+        [SerializableProperty(1)]
+        public int Index { get; set; }
+
+        [SerializableProperty(2)]
+        public int Flags { get; set; }
+    }
+}
