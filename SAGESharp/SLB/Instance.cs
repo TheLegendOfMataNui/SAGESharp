4bc3d35237de23efc7d35c3e301030c38033273a
Vraiment <jemc44@gmail.com>
2019-07-24 00:16:26 -0700

    Add definitions for template SLB_instance.bt

---
+++ SAGESharp/SLB/Instance.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using SAGESharp.IO;
+using System.Collections.Generic;
+
+namespace SAGESharp.SLB
+{
+    public sealed class InstanceTable
+    {
+        [SerializableProperty(1)]
+        public Identifier Id { get; set; }
+
+        [SerializableProperty(2)]
+        [DuplicateEntryCount]
+        public IList<Instance> Entries { get; set; }
+    }
+
+    public sealed class Instance
+    {
+        [SerializableProperty(1)]
+        public Identifier Id1 { get; set; }
+
+        [SerializableProperty(2)]
+        public Identifier Id2 { get; set; }
+
+        [SerializableProperty(3)]
+        [RightPadding(16)]
+        public Point3D Position { get; set; }
+    }
+}
