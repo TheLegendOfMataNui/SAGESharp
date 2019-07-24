4ac7970d04b35aa9ae45145020420d45ffba2783
Vraiment <jemc44@gmail.com>
2019-07-24 09:53:48 -0700

    Add definitions for template SLB_part.bt

---
+++ SAGESharp/SLB/Part.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using SAGESharp.IO;
+using System.Collections.Generic;
+
+namespace SAGESharp.SLB
+{
+    public sealed class PartVector
+    {
+        [SerializableProperty(1)]
+        [DuplicateEntryCount]
+        public IList<Part> Entries { get; set; }
+    }
+
+    public sealed class Part
+    {
+        [SerializableProperty(1)]
+        public float Float1 { get; set; }
+
+        [SerializableProperty(2)]
+        public float Float2 { get; set; }
+
+        [SerializableProperty(3)]
+        public float Float3 { get; set; }
+
+        [SerializableProperty(4)]
+        public float Float4 { get; set; }
+
+        [SerializableProperty(5)]
+        public int Int1 { get; set; }
+
+        [SerializableProperty(6)]
+        public int Int2 { get; set; }
+    }
+}
