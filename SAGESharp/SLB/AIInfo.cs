c27ca4037ae5666838c737e15b7fd47dbed6f20b
Vraiment <jemc44@gmail.com>
2019-07-23 23:59:04 -0700

    Add definitions for template SLB_aiinfo.bt

---
+++ SAGESharp/SLB/AIInfo.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using SAGESharp.IO;
+
+namespace SAGESharp.SLB
+{
+    public sealed class AIInfo
+    {
+        [SerializableProperty(1)]
+        [RightPadding(3)]
+        public bool Flying { get; set; }
+
+        [SerializableProperty(2)]
+        public float Range { get; set; }
+
+        [SerializableProperty(3)]
+        public long PatrolRandomness { get; set; }
+
+        [SerializableProperty(4)]
+        public TimerValues TimerValuesIdle { get; set; }
+
+        [SerializableProperty(5)]
+        public TimerValues TimerValuesPatrol { get; set; }
+
+        [SerializableProperty(6)]
+        public short Toughness { get; set; }
+
+        [SerializableProperty(7)]
+        public AttackType AttackType { get; set; }
+
+        [SerializableProperty(8)]
+        public bool Benign { get; set; }
+
+        [SerializableProperty(9)]
+        [BinaryString(StringPosition.Inline, 32)]
+        public string ProjectileSprite { get; set; }
+    }
+
+    public sealed class TimerValues
+    {
+        [SerializableProperty(1)]
+        public float A { get; set; }
+
+        [SerializableProperty(2)]
+        public float B { get; set; }
+    }
+
+    public enum AttackType : short
+    {
+        Melee = 0,
+        Kamikaze = 1,
+        Jump = 2,
+        Ranged = 3,
+        MoveToAttack = 4,
+        Miniboss = 5
+    }
+}
