28b9d78c3c229909c8aabbcd557543d25e2ce840
Vraiment <jemc44@gmail.com>
2019-07-24 00:11:21 -0700

    Add definitions for template SLB_cylinder.bt

---
+++ SAGESharp/SLB/Cylinder.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using SAGESharp.IO;
+
+namespace SAGESharp.SLB
+{
+    public sealed class Cylinder
+    {
+        [SerializableProperty(1)]
+        public Identifier Id { get; set; }
+
+        [SerializableProperty(2)]
+        public CollisionCylinder CollisionCylinder { get; set; }
+    }
+
+    public sealed class CollisionCylinder
+    {
+        [SerializableProperty(1)]
+        Point3D P1 { get; set; }
+
+        [SerializableProperty(2)]
+        Point3D P2 { get; set; }
+    }
+}
