3f1491a0af0369d8b003e38626c2c9772b296f5a
Vraiment <jemc44@gmail.com>
2019-10-12 01:28:24 -0700

    Implement TreeBuilder class

--- SAGESharp/IO/TreeBasedSerialization.cs
+++ SAGESharp/IO/TreeBasedSerialization.cs
@@ -7,6 +7,8 @@ using Konvenience;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 
 namespace SAGESharp.IO
@@ -637,6 +639,205 @@ namespace SAGESharp.IO
             typeof(IList<T>).IsAssignableFrom(list.GetType()),
             $"List argument is of type {list.GetType().Name} which should implement {typeof(IList<T>).Name} but doesn't."
         );
+    }
+
+    internal static class TreeBuilder
+    {
+        public static IDataNode BuildTreeForType(Type type)
+        {
+            Validate.ArgumentNotNull(nameof(type), type);
+
+            return BuildUserTypeDataNode(type);
+        }
+
+        private static object BuildNodeForType(PropertyInfo propertyInfo)
+        {
+            if (IsPrimitiveType(propertyInfo.PropertyType))
+            {
+                return PrimitiveTypeDataNode(propertyInfo.PropertyType);
+            }
+            else if (typeof(string) == propertyInfo.PropertyType)
+            {
+                return StringDataNode(propertyInfo);
+            }
+            else if (IsListType(propertyInfo.PropertyType))
+            {
+                return BuildListNode(propertyInfo);
+            }
+            else
+            {
+                return BuildUserTypeDataNode(propertyInfo.PropertyType);
+            }
+        }
+
+        #region PrimitiveTypeDataNode
+        private static IDataNode PrimitiveTypeDataNode(Type type)
+        {
+            return (IDataNode)typeof(PrimitiveTypeDataNode<>)
+                .MakeGenericType(type)
+                .GetConstructor(Array.Empty<Type>())
+                .Invoke(Array.Empty<object>());
+        }
+
+        private static bool IsPrimitiveType(Type type)
+        {
+            return type == typeof(byte) || type == typeof(short) || type == typeof(ushort)
+                 || type == typeof(int) || type == typeof(uint) || type == typeof(SLB.Identifier)
+                 || type == typeof(float) || type == typeof(double)
+                 || (type.IsEnum && IsPrimitiveType(Enum.GetUnderlyingType(type)));
+        }
+        #endregion
+
+        #region StringDataNode
+        private static object StringDataNode(PropertyInfo propertyInfo)
+        {
+            OffsetStringAttribute offsetStringAttribute = propertyInfo
+                .GetCustomAttribute<OffsetStringAttribute>();
+
+            InlineStringAttribute inlineStringAttribute = propertyInfo
+                .GetCustomAttribute<InlineStringAttribute>();
+
+            if (offsetStringAttribute != null && inlineStringAttribute != null)
+            {
+                throw new BadTypeException(propertyInfo.DeclaringType,
+                    $"Property {propertyInfo.Name} has duplicate string location attributes.");
+            }
+            else if (offsetStringAttribute != null)
+            {
+                return new OffsetNode(new StringDataNode());
+            }
+            else if (inlineStringAttribute != null)
+            {
+                return new StringDataNode((byte)inlineStringAttribute.Length);
+            }
+            else
+            {
+                throw new BadTypeException(propertyInfo.DeclaringType,
+                    $"Property {propertyInfo.Name} is missing a string location attribute.");
+            }
+        }
+        #endregion
+
+        #region UserTypeDataNode
+        private static IDataNode BuildUserTypeDataNode(Type type)
+        {
+            ValidateUserTypeHasConstructor(type);
+
+            IReadOnlyList<IEdge> edges = type.GetProperties()
+                .Select(p => SerializableProperty.For(p))
+                .Where(p => p != null)
+                .OrderBy(p => p.Order)
+                .Also(ps => ValidateSerializableProperties(type, ps))
+                .Select(p => BuildEdge(p.PropertyInfo))
+                .ToList();
+
+            return (IDataNode)typeof(UserTypeDataNode<>)
+                .MakeGenericType(type)
+                .GetConstructor(new Type[] { typeof(IReadOnlyList<IEdge>) }).Invoke(new object[] { edges });
+        }
+
+        private static void ValidateUserTypeHasConstructor(Type type)
+        {
+            if (type.GetConstructor(Array.Empty<Type>()) is null)
+            {
+                throw new BadTypeException(type, "Type doesn't have a public parameterless constructor.");
+            }
+        }
+
+        private static void ValidateSerializableProperties(Type type, IOrderedEnumerable<SerializableProperty> properties)
+        {
+            int count = properties.Count();
+            if (count == 0)
+            {
+                throw new BadTypeException(type, "Type doesn't have serializable properties.");
+            }
+            else if (count != properties.Select(p => p.Order).Distinct().Count())
+            {
+                throw new BadTypeException(type, "Type has two or more properties with the same serialization order.");
+            }
+        }
+
+        private class SerializableProperty
+        {
+            private SerializableProperty(PropertyInfo propertyInfo, byte order)
+            {
+                PropertyInfo = propertyInfo;
+                Order = order;
+            }
+
+            public PropertyInfo PropertyInfo { get; }
+
+            public byte Order { get; }
+
+            public static SerializableProperty For(PropertyInfo propertyInfo) => propertyInfo
+                .GetCustomAttribute<SerializablePropertyAttribute>()
+                ?.Let(attribute => new SerializableProperty(propertyInfo, attribute.BinaryOrder));
+        }
+        #endregion
+
+        #region Edge
+        private static IEdge BuildEdge(PropertyInfo propertyInfo)
+        {
+            Type edgeType = typeof(Edge<>).MakeGenericType(propertyInfo.DeclaringType);
+            Type extractorType = typeof(Func<,>).MakeGenericType(propertyInfo.DeclaringType, typeof(object));
+            Type setterType = typeof(Action<,>).MakeGenericType(propertyInfo.DeclaringType, typeof(object));
+
+            object extractor = GetExtractorForEdge(propertyInfo);
+            object setter = GetSetterForEdge(propertyInfo);
+            object childNode = BuildNodeForType(propertyInfo);
+
+            return (IEdge)edgeType.GetConstructor(new Type[] { extractorType, setterType, typeof(object) })
+                .Invoke(new object[] { extractor, setter, childNode });
+        }
+
+        private static object GetExtractorForEdge(PropertyInfo propertyInfo)
+        {
+            ParameterExpression valueParameter = Expression.Parameter(propertyInfo.DeclaringType);
+
+            MethodCallExpression methodCall = Expression.Call(valueParameter, propertyInfo.GetMethod);
+
+            UnaryExpression castedMethodCall = Expression.Convert(methodCall, typeof(object));
+
+            LambdaExpression lambdaExpression = Expression.Lambda(castedMethodCall, valueParameter);
+
+            return lambdaExpression.Compile();
+        }
+
+        private static object GetSetterForEdge(PropertyInfo propertyInfo)
+        {
+            ParameterExpression valueParameter = Expression.Parameter(propertyInfo.DeclaringType);
+            ParameterExpression childValueParameter = Expression.Parameter(typeof(object));
+            UnaryExpression castedChildValueParameter = Expression.Convert(childValueParameter, propertyInfo.PropertyType);
+
+            MethodCallExpression methodCall = Expression.Call(valueParameter, propertyInfo.SetMethod, castedChildValueParameter);
+
+            LambdaExpression lambdaExpression = Expression.Lambda(methodCall, valueParameter, childValueParameter);
+
+            return lambdaExpression.Compile();
+        }
+        #endregion
+
+        #region ListNode
+        private static object BuildListNode(PropertyInfo propertyInfo)
+        {
+            Type typeOfList = propertyInfo.PropertyType.GenericTypeArguments[0];
+
+            object childNode = BuildUserTypeDataNode(typeOfList);
+
+            bool duplicateEntryCount = propertyInfo
+                .GetCustomAttribute<DuplicateEntryCountAttribute>() != null;
+
+            return typeof(ListNode<>)
+                .MakeGenericType(typeOfList)
+                .GetConstructor(new Type[] { typeof(IDataNode), typeof(bool) })
+                .Invoke(new object[] { childNode, duplicateEntryCount });
+        }
+
+        private static bool IsListType(Type type)
+        {
+            return type.IsGenericType && typeof(IList<>) == type.GetGenericTypeDefinition();
+        }
+        #endregion
     }
 
     internal sealed class TreeReader : ITreeReader
---
+++ SAGESharpTests/IO/TreeBuilderTests.cs
@@ -0,0 +1,391 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using FluentAssertions;
+using NUnit.Framework;
+using SAGESharp.Testing;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SAGESharp.IO
+{
+    class TreeBuilderTests
+    {
+        #region Building UserTypeDataNode
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_With_Primitive_Properties()
+        {
+            IDataNode rootNode = TreeBuilder.BuildTreeForType(typeof(ClassWithPrimitiveProperties));
+
+            ClassWithPrimitiveProperties.BuildSample().VerifyTree(rootNode);
+        }
+
+        private sealed class ClassWithPrimitiveProperties
+        {
+            [SerializableProperty(0)]
+            public int Int { get; set; }
+
+            [SerializableProperty(1)]
+            public float Float { get; set; }
+
+            public void VerifyTree(IDataNode dataNode)
+            {
+                dataNode.Should().BeOfType<UserTypeDataNode<ClassWithPrimitiveProperties>>();
+
+                dataNode.Edges.Should().HaveCount(2);
+
+                VerifyEdge(dataNode.Edges[0], this, v => v.Int);
+
+                dataNode.Edges[0].ChildNode.Should().BeOfType<PrimitiveTypeDataNode<int>>();
+
+                VerifyEdge(dataNode.Edges[1], this, v => v.Float);
+
+                dataNode.Edges[1].ChildNode.Should().BeOfType<PrimitiveTypeDataNode<float>>();
+            }
+
+            public static ClassWithPrimitiveProperties BuildSample() => new ClassWithPrimitiveProperties
+            {
+                Int = 32,
+                Float = 9.2f
+            };
+        }
+
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_With_Nested_Class_With_Primitive_Properties()
+        {
+            IDataNode rootNode = TreeBuilder.BuildTreeForType(typeof(ClassWithNestedClassWithPrimitiveProperties));
+
+            ClassWithNestedClassWithPrimitiveProperties.BuildSample().VerifyTree(rootNode);
+        }
+
+        private sealed class ClassWithNestedClassWithPrimitiveProperties
+        {
+            [SerializableProperty(0)]
+            public ClassWithPrimitiveProperties FlatClass { get; set; }
+
+            [SerializableProperty(1)]
+            public byte Byte { get; set; }
+
+            [SerializableProperty(2)]
+            public SLB.Identifier Identifier { get; set; }
+
+            public void VerifyTree(IDataNode dataNode)
+            {
+                dataNode.Should().BeOfType<UserTypeDataNode<ClassWithNestedClassWithPrimitiveProperties>>();
+
+                dataNode.Edges.Should().HaveCount(3);
+
+                VerifyEdge(dataNode.Edges[0], this, v => v.FlatClass);
+
+                FlatClass.VerifyTree(dataNode.Edges[0].ChildNode as IDataNode);
+
+                VerifyEdge(dataNode.Edges[1], this, v => v.Byte);
+
+                dataNode.Edges[1].ChildNode.Should().BeOfType<PrimitiveTypeDataNode<byte>>();
+
+                VerifyEdge(dataNode.Edges[2], this, v => v.Identifier);
+
+                dataNode.Edges[2].ChildNode.Should().BeOfType<PrimitiveTypeDataNode<SLB.Identifier>>();
+            }
+
+            public static ClassWithNestedClassWithPrimitiveProperties BuildSample() => new ClassWithNestedClassWithPrimitiveProperties
+            {
+                FlatClass = ClassWithPrimitiveProperties.BuildSample(),
+                Byte = 12
+            };
+        }
+        #endregion
+
+        #region Building ListNode
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_With_A_List()
+        {
+            IDataNode rootNode = TreeBuilder.BuildTreeForType(typeof(ClassWithAList));
+
+            VerifyTreeForClassWithAList(
+                dataNode: rootNode,
+                expected: new ClassWithAList
+                {
+                    Values = new List<ClassWithPrimitiveProperties>()
+                },
+                extractor: v => v.Values,
+                duplicateEntryCount: false
+            );
+        }
+
+        private sealed class ClassWithAList
+        {
+            [SerializableProperty(0)]
+            public IList<ClassWithPrimitiveProperties> Values { get; set; }
+        }
+
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_With_A_List_With_Duplicated_Entry_Count()
+        {
+            IDataNode rootNode = TreeBuilder.BuildTreeForType(typeof(ClassWithAListWithDuplicatedEntryCount));
+
+            VerifyTreeForClassWithAList(
+                dataNode: rootNode,
+                expected: new ClassWithAListWithDuplicatedEntryCount
+                {
+                    Values = new List<ClassWithPrimitiveProperties>()
+                },
+                extractor: v => v.Values,
+                duplicateEntryCount: true
+            );
+        }
+
+        private sealed class ClassWithAListWithDuplicatedEntryCount
+        {
+            [SerializableProperty(0)]
+            [DuplicateEntryCount]
+            public IList<ClassWithPrimitiveProperties> Values { get; set; }
+        }
+
+        private static void VerifyTreeForClassWithAList<T>(IDataNode dataNode, T expected, Func<T, object> extractor, bool duplicateEntryCount) where T : new()
+        {
+            dataNode.Should().BeOfType<UserTypeDataNode<T>>();
+
+            dataNode.Edges.Should().HaveCount(1);
+
+            VerifyEdge(dataNode.Edges[0], expected, extractor);
+
+            ListNode<ClassWithPrimitiveProperties> listNode = dataNode.Edges[0].ChildNode.Should()
+                .BeOfType<ListNode<ClassWithPrimitiveProperties>>()
+                .Subject;
+
+            FieldInfo duplicateEntryCountField = typeof(ListNode<ClassWithPrimitiveProperties>)
+                .GetField("duplicateEntryCount", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            duplicateEntryCountField.GetValue(listNode).Should().Be(duplicateEntryCount);
+
+            ClassWithPrimitiveProperties.BuildSample().VerifyTree(listNode.ChildNode);
+        }
+        #endregion
+
+        #region Building StringDataNode
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_With_A_String_At_Offset()
+        {
+            IDataNode rootNode = TreeBuilder.BuildTreeForType(typeof(ClassWithStringAtOffset));
+
+            ClassWithStringAtOffset.BuildSample().VerifyTree(rootNode);
+        }
+
+        private sealed class ClassWithStringAtOffset
+        {
+            [SerializableProperty(0)]
+            [OffsetString]
+            public string Value { get; set; }
+
+            public void VerifyTree(IDataNode dataNode)
+            {
+                dataNode.Should().BeOfType<UserTypeDataNode<ClassWithStringAtOffset>>();
+
+                dataNode.Edges.Should().HaveCount(1);
+
+                VerifyEdge(dataNode.Edges[0], this, v => v.Value);
+
+                StringDataNode stringDataNode = dataNode.Edges[0].ChildNode.Should().BeOfType<OffsetNode>()
+                    .Subject.ChildNode.Should().BeOfType<StringDataNode>()
+                    .Subject;
+
+                GetLengthForStringDataNode(stringDataNode).Should().Be(StringDataNode.OFFSET_STRING_MAX_LENGTH);
+            }
+
+            public static ClassWithStringAtOffset BuildSample() => new ClassWithStringAtOffset
+            {
+                Value = "This string is at offset"
+            };
+        }
+
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_With_A_String_Inline()
+        {
+            IDataNode rootNode = TreeBuilder.BuildTreeForType(typeof(ClassWithStringInline));
+
+            ClassWithStringInline.BuildSample().VerifyTree(rootNode);
+        }
+
+        private sealed class ClassWithStringInline
+        {
+            private const int LENGTH = 27;
+
+            [SerializableProperty(0)]
+            [InlineString(LENGTH)]
+            public string Value { get; set; }
+
+            public void VerifyTree(IDataNode dataNode)
+            {
+                dataNode.Should().BeOfType<UserTypeDataNode<ClassWithStringInline>>();
+
+                dataNode.Edges.Should().HaveCount(1);
+
+                VerifyEdge(dataNode.Edges[0], this, v => v.Value);
+
+                StringDataNode stringDataNode = dataNode.Edges[0].ChildNode.Should().BeOfType<StringDataNode>()
+                    .Subject;
+
+                GetLengthForStringDataNode(stringDataNode).Should().Be(LENGTH);
+            }
+
+            public static ClassWithStringInline BuildSample() => new ClassWithStringInline
+            {
+                Value = "This string is inline"
+            };
+        }
+
+        private static int GetLengthForStringDataNode(StringDataNode stringDataNode)
+        {
+            FieldInfo field = typeof(StringDataNode)
+                .GetField("length", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            return (byte)field.GetValue(stringDataNode);
+        }
+        #endregion
+
+        #region Error checking
+        [Test]
+        public void Test_Building_A_Tree_For_A_Null_Type()
+        {
+            Action action = () => TreeBuilder.BuildTreeForType(null);
+
+            action.Should()
+                .ThrowArgumentNullException("type");
+        }
+
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_With_Private_Parameterless_Constructor()
+        {
+            BadTypeExceptionShouldBeThrownForType<ClassWithPrivateParameterlessConstructor>
+            (
+                message: $"Type doesn't have a public parameterless constructor."
+            );
+        }
+
+        private class ClassWithPrivateParameterlessConstructor
+        {
+            private ClassWithPrivateParameterlessConstructor()
+            {
+            }
+        }
+
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_Without_Parameterless_Constructor()
+        {
+            BadTypeExceptionShouldBeThrownForType<ClassWithoutParameterlessConstructor>
+            (
+                message: $"Type doesn't have a public parameterless constructor."
+            );
+        }
+
+        private class ClassWithoutParameterlessConstructor
+        {
+            public ClassWithoutParameterlessConstructor(int value)
+            {
+            }
+        }
+
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_Without_Serializable_Properties()
+        {
+            BadTypeExceptionShouldBeThrownForType<ClassWithoutSerializableProperties>
+            (
+                message: $"Type doesn't have serializable properties."
+            );
+        }
+
+        private class ClassWithoutSerializableProperties
+        {
+            public int Int { get; set; }
+        }
+
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_With_Properties_With_Duplicate_Serialization_Order()
+        {
+            BadTypeExceptionShouldBeThrownForType<ClassWithPropertiesWithDuplicateSerializationOrder>
+            (
+                message: $"Type has two or more properties with the same serialization order."
+            );
+        }
+
+        private class ClassWithPropertiesWithDuplicateSerializationOrder
+        {
+            [SerializableProperty(0)]
+            public int Int { get; set; }
+
+            [SerializableProperty(0)]
+            public float Float { get; set; }
+        }
+
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_With_String_Property_Without_String_Attribute()
+        {
+            string propertyName = nameof(ClassWithStringPropertyWithoutStringAttribute.Value);
+
+            BadTypeExceptionShouldBeThrownForType<ClassWithStringPropertyWithoutStringAttribute>
+            (
+                message: $"Property {propertyName} is missing a string location attribute."
+            );
+        }
+
+        private class ClassWithStringPropertyWithoutStringAttribute
+        {
+            [SerializableProperty(0)]
+            public string Value { get; set; }
+        }
+
+        [Test]
+        public void Test_Building_A_Tree_For_A_Class_With_String_Property_With_Both_String_Attributes()
+        {
+            string propertyName = nameof(ClassWithStringPropertyWithBothStringAttributes.Value);
+            
+            BadTypeExceptionShouldBeThrownForType<ClassWithStringPropertyWithBothStringAttributes>
+            (
+                message: $"Property {propertyName} has duplicate string location attributes."
+            );
+        }
+
+        private class ClassWithStringPropertyWithBothStringAttributes
+        {
+            [SerializableProperty(0)]
+            [OffsetString]
+            [InlineString(39)]
+            public string Value { get; set; }
+        }
+        #endregion
+
+        private static void BadTypeExceptionShouldBeThrownForType<T>(string message)
+        {
+            Action action = () => TreeBuilder.BuildTreeForType(typeof(T));
+
+            action.Should()
+                .ThrowExactly<BadTypeException>()
+                .WithMessage(message)
+                .Where(e => typeof(T) == e.Type);
+        }
+
+        private static void VerifyEdge<T>(IEdge edge, T expected, Func<T, object> extractor) where T : new()
+        {
+            Edge<T> actualEdge = edge.Should().BeOfType<Edge<T>>().Subject;
+            T value = new T();
+
+            actualEdge.SetChildValue(value, extractor(expected));
+
+            extractor(value).Should().BeEquivalentTo(extractor(expected));
+
+            object childValue = actualEdge.ExtractChildValue(value);
+            if (childValue.GetType().IsValueType)
+            {
+                childValue.Should().BeEquivalentTo(extractor(expected));
+            }
+            else
+            {
+                childValue.Should().BeSameAs(extractor(expected));
+            }
+        }
+    }
+}
