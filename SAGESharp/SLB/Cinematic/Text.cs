69ea285a6d22a630b284e077ab6d3085f71c162f
Vraiment <jemc44@gmail.com>
2020-01-14 08:43:58 -0800

    Use Equ in classes from SAGESharp/SLB/Cinematic/Text.cs

--- SAGESharp/SLB/Cinematic/Text.cs
+++ SAGESharp/SLB/Cinematic/Text.cs
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+using Equ;
 using NUtils.Extensions;
 using SAGESharp.IO;
 using System;
@@ -22,16 +23,7 @@ namespace SAGESharp.SLB.Cinematic
         public IList<Data> Entries { get; set; }
 
         public bool Equals(Text other)
-        {
-            if (other == null)
-            {
-                return false;
-            }
-
-            return StrLabel == other.StrLabel &&
-                StrIdx == other.StrIdx &&
-                Entries.SafeSequenceEquals(other.Entries);
-        }
+            => MemberwiseEqualityComparer<Text>.ByProperties.Equals(this, other);
 
         public override string ToString() => $"StrLabel={StrLabel}," +
             $"StrIdx={StrIdx}," +
@@ -41,30 +33,10 @@ namespace SAGESharp.SLB.Cinematic
             => Equals(other as Text);
 
         public override int GetHashCode()
-        {
-            int hash = 3299;
-            StrLabel.AddHashCodeByVal(ref hash, 2957);
-            StrIdx.AddHashCodeByVal(ref hash, 2957);
-            Entries.AddHashCodesByRef(ref hash, 2957, 4093);
-
-            return hash;
-        }
+            => MemberwiseEqualityComparer<Text>.ByProperties.GetHashCode(this);
 
         public static bool operator ==(Text left, Text right)
-        {
-            if (ReferenceEquals(left, right))
-            {
-                return true;
-            }
-            else if (left is null)
-            {
-                return right.Equals(left);
-            }
-            else
-            {
-                return left.Equals(right);
-            }
-        }
+            => left?.Equals(right) ?? right?.Equals(left) ?? true;
 
         public static bool operator !=(Text left, Text right)
             => !(left == right);
@@ -79,15 +51,7 @@ namespace SAGESharp.SLB.Cinematic
         public long StrOffset { get; set; }
 
         public bool Equals(Data other)
-        {
-            if (other == null)
-            {
-                return false;
-            }
-
-            return Time == other.Time &&
-                StrOffset == other.StrOffset;
-        }
+            => MemberwiseEqualityComparer<Data>.ByProperties.Equals(this, other);
 
         public override string ToString() => $"Time={Time}," +
             $"StrOffset={StrOffset},";
@@ -96,29 +60,10 @@ namespace SAGESharp.SLB.Cinematic
             => Equals(other as Data);
 
         public override int GetHashCode()
-        {
-            int hash = 9857;
-            Time.AddHashCodeByVal(ref hash, 4523);
-            StrOffset.AddHashCodeByVal(ref hash, 4523);
-
-            return hash;
-        }
+            => MemberwiseEqualityComparer<Data>.ByProperties.GetHashCode(this);
 
         public static bool operator ==(Data left, Data right)
-        {
-            if (ReferenceEquals(left, right))
-            {
-                return true;
-            }
-            else if (left is null)
-            {
-                return right.Equals(left);
-            }
-            else
-            {
-                return left.Equals(right);
-            }
-        }
+            => left?.Equals(right) ?? right?.Equals(left) ?? true;
 
         public static bool operator !=(Data left, Data right)
             => !(left == right);
