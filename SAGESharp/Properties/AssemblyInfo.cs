439491f2171979b39934938ab9d776af96bdc664
Vraiment <jemc44@gmail.com>
2020-01-18 15:29:16 -0800

    Update assembly version

--- SAGESharp/Properties/AssemblyInfo.cs
+++ SAGESharp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Litestone Studios")]
 [assembly: AssemblyProduct("SAGESharp")]
-[assembly: AssemblyCopyright("Copyright © Litestone 2018")]
+[assembly: AssemblyCopyright("Copyright © Litestone 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
 
 // The following lines make internal classes visible to tests and also
 // the mocking framework. We don't want the signed assembly to expose
