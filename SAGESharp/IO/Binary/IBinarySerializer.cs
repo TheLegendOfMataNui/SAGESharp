28b5f4a205f8e6969ffc55ab2c178fc346060100
Vraiment <jemc44@gmail.com>
2020-02-16 15:03:22 -0800

    Move IBinarySerializer interface to the correct namespace

---
+++ SAGESharp/IO/Binary/IBinarySerializer.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using System;
+
+namespace SAGESharp.IO.Binary
+{
+    /// <summary>
+    /// Represents an object that serializes objects of type <typeparamref name="T"/>.
+    /// </summary>
+    /// 
+    /// <typeparam name="T">Type of the objects to serialize.</typeparam>
+    public interface IBinarySerializer<T>
+    {
+        /// <summary>
+        /// Reads an object of type <typeparamref name="T"/> from the input <paramref name="binaryReader"/>.
+        /// </summary>
+        /// 
+        /// <param name="binaryReader">The binary reader used to read the object.</param>
+        /// 
+        /// <returns>The object read from the <paramref name="binaryReader"/>.</returns>
+        /// 
+        /// <exception cref="ArgumentNullException">If <paramref name="binaryReader"/> is null.</exception>
+        T Read(IBinaryReader binaryReader);
+
+        /// <summary>
+        /// Writes <paramref name="value"/> to the output <paramref name="binaryWriter"/>.
+        /// </summary>
+        /// 
+        /// <param name="binaryWriter">The output binary writer were the object will be written.</param>
+        /// <param name="value">The object to write.</param>
+        /// 
+        /// <exception cref="ArgumentNullException">If either <paramref name="binaryWriter"/> or <paramref name="value"/> are null.</exception>
+        void Write(IBinaryWriter binaryWriter, T value);
+    }
+}
--- SAGESharp/IO/BinarySerializers.cs
+++ SAGESharp/IO/BinarySerializers.cs
@@ -11,37 +11,6 @@ using System.Collections.Generic;
 
 namespace SAGESharp.IO
 {
-    #region Interface
-    /// <summary>
-    /// Represents an object that serializes objects of type <typeparamref name="T"/>.
-    /// </summary>
-    /// 
-    /// <typeparam name="T">Type of the objects to serialize.</typeparam>
-    public interface IBinarySerializer<T>
-    {
-        /// <summary>
-        /// Reads an object of type <typeparamref name="T"/> from the input <paramref name="binaryReader"/>.
-        /// </summary>
-        /// 
-        /// <param name="binaryReader">The binary reader used to read the object.</param>
-        /// 
-        /// <returns>The object read from the <paramref name="binaryReader"/>.</returns>
-        /// 
-        /// <exception cref="ArgumentNullException">If <paramref name="binaryReader"/> is null.</exception>
-        T Read(IBinaryReader binaryReader);
-
-        /// <summary>
-        /// Writes <paramref name="value"/> to the output <paramref name="binaryWriter"/>.
-        /// </summary>
-        /// 
-        /// <param name="binaryWriter">The output binary writer were the object will be written.</param>
-        /// <param name="value">The object to write.</param>
-        /// 
-        /// <exception cref="ArgumentNullException">If either <paramref name="binaryWriter"/> or <paramref name="value"/> are null.</exception>
-        void Write(IBinaryWriter binaryWriter, T value);
-    }
-    #endregion
-
     #region Implementation
     internal sealed class TreeBinarySerializer<T> : IBinarySerializer<T>
     {
