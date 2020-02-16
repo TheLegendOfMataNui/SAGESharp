492e653b050143f9e68acda1a2fae999424edcfe
Vraiment <jemc44@gmail.com>
2020-02-16 15:01:28 -0800

    Move IBinarySerializable interface to the correct namespace

---
+++ SAGESharp/IO/Binary/IBinarySerializable.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+namespace SAGESharp.IO.Binary
+{
+    /// <summary>
+    /// Represents an object that can serialize itself.
+    /// </summary>
+    public interface IBinarySerializable
+    {
+        /// <summary>
+        /// Reads data from the <paramref name="binaryReader"/> into the object itself.
+        /// </summary>
+        /// 
+        /// <param name="binaryReader">The input reader.</param>
+        void Read(IBinaryReader binaryReader);
+
+        /// <summary>
+        /// Writes data to the <paramref name="binaryWriter"/> from the object itself.
+        /// </summary>
+        /// 
+        /// <param name="binaryWriter">The output writer.</param>
+        void Write(IBinaryWriter binaryWriter);
+    }
+}
