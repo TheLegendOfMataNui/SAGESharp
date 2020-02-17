4cd5362673e7529e7221d65785989494d5f00e4e
Vraiment <jemc44@gmail.com>
2020-02-16 22:28:42 -0800

    Move IOffsetNode interface to the correct namespace

---
+++ SAGESharp/IO/Binary/TreeBasedSerialization/IOffsetNode.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+using System;
+
+namespace SAGESharp.IO.Binary.TreeBasedSerialization
+{
+    /// <summary>
+    /// Represents a node that will write its contents at a later time.
+    /// </summary>
+    internal interface IOffsetNode
+    {
+        /// <summary>
+        /// Reads the offset location at which the value of <see cref="ChildNode"/> is located.
+        /// </summary>
+        /// 
+        /// <param name="binaryReader">The input binary reader to read the offset from.</param>
+        /// 
+        /// <returns>The offset of value of <see cref="ChildNode"/>.</returns>
+        uint ReadOffset(IBinaryReader binaryReader);
+
+        /// <summary>
+        /// Writes <paramref name="value"/> to the given <paramref name="binaryWriter"/>.
+        /// </summary>
+        /// 
+        /// <param name="binaryWriter">The output binary writer were the object will be written.</param>
+        /// <param name="value">The value to be written.</param>
+        /// 
+        /// <returns>The position where the offset was written.</returns>
+        /// 
+        /// <exception cref="ArgumentNullException">
+        /// If either <paramref name="binaryWriter"/> or <paramref name="value"/> are null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If <paramref name="value"/> is not of the expected type.
+        /// </exception>
+        uint Write(IBinaryWriter binaryWriter, object value);
+
+        /// <summary>
+        /// The actual node that will write the contents of the object.
+        /// </summary>
+        IDataNode ChildNode { get; }
+    }
+}
--- SAGESharp/IO/TreeBasedSerialization.cs
+++ SAGESharp/IO/TreeBasedSerialization.cs
@@ -48,43 +48,6 @@ namespace SAGESharp.IO
     }
 
     /// <summary>
-    /// Represents a node that will write its contents at a later time.
-    /// </summary>
-    internal interface IOffsetNode
-    {
-        /// <summary>
-        /// Reads the offset location at which the value of <see cref="ChildNode"/> is located.
-        /// </summary>
-        /// 
-        /// <param name="binaryReader">The input binary reader to read the offset from.</param>
-        /// 
-        /// <returns>The offset of value of <see cref="ChildNode"/>.</returns>
-        uint ReadOffset(IBinaryReader binaryReader);
-
-        /// <summary>
-        /// Writes <paramref name="value"/> to the given <paramref name="binaryWriter"/>.
-        /// </summary>
-        /// 
-        /// <param name="binaryWriter">The output binary writer were the object will be written.</param>
-        /// <param name="value">The value to be written.</param>
-        /// 
-        /// <returns>The position where the offset was written.</returns>
-        /// 
-        /// <exception cref="ArgumentNullException">
-        /// If either <paramref name="binaryWriter"/> or <paramref name="value"/> are null.
-        /// </exception>
-        /// <exception cref="ArgumentException">
-        /// If <paramref name="value"/> is not of the expected type.
-        /// </exception>
-        uint Write(IBinaryWriter binaryWriter, object value);
-
-        /// <summary>
-        /// The actual node that will write the contents of the object.
-        /// </summary>
-        IDataNode ChildNode { get; }
-    }
-
-    /// <summary>
     /// Represents an edge connecting a <see cref="IDataNode"/> to
     /// a node (ex: <see cref="IDataNode"/>, <see cref="IOffsetNode"/>).
     /// </summary>
