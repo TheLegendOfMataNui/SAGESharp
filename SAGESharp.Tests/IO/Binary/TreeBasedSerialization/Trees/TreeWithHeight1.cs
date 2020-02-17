041c25b18f124733ad655bcdbda9a4c8e86f81ee
Vraiment <jemc44@gmail.com>
2020-02-16 22:33:34 -0800

    Move IDataNode interface to the correct namespace

--- SAGESharp.Tests/IO/Binary/TreeBasedSerialization/Trees/TreeWithHeight1.cs
+++ SAGESharp.Tests/IO/Binary/TreeBasedSerialization/Trees/TreeWithHeight1.cs
@@ -4,6 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 using SAGESharp.IO;
+using SAGESharp.IO.Binary.TreeBasedSerialization;
 using System.Collections.Generic;
 
 namespace SAGESharp.Tests.IO.Binary.TreeBasedSerialization.Trees
--- SAGESharp/IO/TreeBasedSerialization.cs
+++ SAGESharp/IO/TreeBasedSerialization.cs
@@ -3,50 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-using SAGESharp.IO.Binary;
 using System;
-using System.Collections.Generic;
 
 namespace SAGESharp.IO
 {
     #region Interfaces
-    /// <summary>
-    /// Represents a node with data (with children nodes) in the tree.
-    /// </summary>
-    internal interface IDataNode
-    {
-        /// <summary>
-        /// Reads a value from the <paramref name="binaryReader"/>.
-        /// </summary>
-        /// 
-        /// <param name="binaryReader">The reader that will be used to read the value.</param>
-        /// 
-        /// <returns>The value read from <paramref name="binaryReader"/>.</returns>
-        /// 
-        /// <exception cref="ArgumentNullException">If <paramref name="binaryReader"/> is null.</exception>
-        object Read(IBinaryReader binaryReader);
-
-        /// <summary>
-        /// Writes <paramref name="value"/> to the given <paramref name="binaryWriter"/>.
-        /// </summary>
-        /// 
-        /// <param name="binaryWriter">The output binary writer were the object will be written.</param>
-        /// <param name="value">The value to be written.</param>
-        /// 
-        /// <exception cref="ArgumentNullException">
-        /// If either <paramref name="binaryWriter"/> or <paramref name="value"/> are null.
-        /// </exception>
-        /// <exception cref="ArgumentException">
-        /// If <paramref name="value"/> is not of the expected type.
-        /// </exception>
-        void Write(IBinaryWriter binaryWriter, object value);
-
-        /// <summary>
-        /// Returns the list of edges connecting to child nodes.
-        /// </summary>
-        IReadOnlyList<IEdge> Edges { get; }
-    }
-
     /// <summary>
     /// Represents an edge connecting a <see cref="IDataNode"/> to
     /// a node (ex: <see cref="IDataNode"/>, <see cref="IOffsetNode"/>).
