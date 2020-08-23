b0bf0f6a43b77002e26add2b747908fb2f36fb32
Vraiment <jemc44@gmail.com>
2020-08-22 17:34:32 -0700

    Update quaternion properties to be floats

--- SAGESharp/Animations/TCBQuaternionData.cs
+++ SAGESharp/Animations/TCBQuaternionData.cs
@@ -11,39 +11,62 @@ namespace SAGESharp.Animations
 {
     public sealed class TCBQuaternionData : IEquatable<TCBQuaternionData>, IBinarySerializable
     {
+        private static readonly float FLOAT_CONVERSION_CONSTANT = 1 / (float)Math.Pow(2, 15);
+
+        private ushort x;
+        private ushort y;
+        private ushort z;
+        private ushort w;
+
         public short Keyframe { get; set; }
 
-        public short X { get; set; }
+        public float X
+        {
+            get => x * FLOAT_CONVERSION_CONSTANT;
+            set => x = (ushort)(value / FLOAT_CONVERSION_CONSTANT);
+        }
 
-        public short Y { get; set; }
+        public float Y
+        {
+            get => y * FLOAT_CONVERSION_CONSTANT;
+            set => y = (ushort)(value / FLOAT_CONVERSION_CONSTANT);
+        }
 
-        public short Z { get; set; }
+        public float Z
+        {
+            get => z * FLOAT_CONVERSION_CONSTANT;
+            set => z = (ushort)(value / FLOAT_CONVERSION_CONSTANT);
+        }
 
-        public short W { get; set; }
+        public float W
+        {
+            get => w * FLOAT_CONVERSION_CONSTANT;
+            set => w = (ushort)(value / FLOAT_CONVERSION_CONSTANT);
+        }
 
         #region IBinarySerializable
         public void Read(IBinaryReader binaryReader)
         {
             short keyframe = binaryReader.ReadInt16();
-            short x = binaryReader.ReadInt16();
-            short y = binaryReader.ReadInt16();
-            short z = binaryReader.ReadInt16();
-            short w = binaryReader.ReadInt16();
+            ushort x = binaryReader.ReadUInt16();
+            ushort y = binaryReader.ReadUInt16();
+            ushort z = binaryReader.ReadUInt16();
+            ushort w = binaryReader.ReadUInt16();
 
             Keyframe = keyframe;
-            X = x;
-            Y = y;
-            Z = z;
-            W = w;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
         }
 
         public void Write(IBinaryWriter binaryWriter)
         {
             binaryWriter.WriteInt16(Keyframe);
-            binaryWriter.WriteInt16(X);
-            binaryWriter.WriteInt16(Y);
-            binaryWriter.WriteInt16(Z);
-            binaryWriter.WriteInt16(W);
+            binaryWriter.WriteUInt16(x);
+            binaryWriter.WriteUInt16(y);
+            binaryWriter.WriteUInt16(z);
+            binaryWriter.WriteUInt16(w);
         }
         #endregion
 
